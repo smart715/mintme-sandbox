151900595bc5e8b98347e8fd34ceec4bd823c224
Adel Armand <adel.armand@hotmail.com>
2018-07-27 20:12:41 +0430

    [syntax,php,js] Correct syntax

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -1,7 +1,12 @@
-// Require all images so that webpack will handle them and add to manifest.json,
+// Require all images so that webpack will handle
+// them and add to manifest.json,
 // as well as move them to public/build/,
 // so that later Asset component will be able to use asset versioning
-// based on the manifest.json (see assets section in config/packages/framework.yaml),
+// based on the manifest.json
+// (see assets section in config/packages/framework.yaml),
 // and Twig could properly use asset() function for images.
-const imagesContext = require.context('../img', false, /\.(png|jpg|jpeg|gif|ico|svg)$/);
+const imagesContext = require.context(
+  '../img', false,
+  /\.(png|jpg|jpeg|gif|ico|svg)$/
+);
 imagesContext.keys().forEach(imagesContext);
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('default/index.html.twig');
     }
@@ -18,7 +19,7 @@ class DefaultController extends Controller
     /**
      * @Route("/trading", name="trading")
      */
-    public function trading()
+    public function trading(): Response
     {
         return $this->render('default/trading.html.twig');
     }
