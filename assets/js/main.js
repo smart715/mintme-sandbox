88f69f7edf4146c34df23f1b3a42dcb0100c4963
bigshan <segun.adeniji01@gmail.com>
2018-08-02 07:16:37 +0100

    use bootstrap vue navbar in the base.html.twig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 import '../scss/main.sass';
 import bDropdown from 'bootstrap-vue/es/components/dropdown/dropdown';
 import bDropdownItem from 'bootstrap-vue/es/components/dropdown/dropdown-item';
+import bNavbar from 'bootstrap-vue/es/components/navbar/navbar';
+import bNavbarNav from 'bootstrap-vue/es/components/navbar/navbar-nav';
+import bNavbarBrand from 'bootstrap-vue/es/components/navbar/navbar-brand';
+import bNavbarToggle from 'bootstrap-vue/es/components/navbar/navbar-toggle';
+import bNavItem from 'bootstrap-vue/es/components/nav/nav-item';
+import bCollapse from 'bootstrap-vue/es/components/collapse/collapse';
 window.Vue = require('vue');
 
 Vue.options.delimiters = ['{[', ']}'];
@@ -10,6 +16,12 @@ new Vue({
     components: {
         bDropdown,
         bDropdownItem,
+        bNavbar,
+        bNavbarNav,
+        bNavbarBrand,
+        bNavbarToggle,
+        bNavItem,
+        bCollapse,
     },
 });
 
