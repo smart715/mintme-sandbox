d981f755768c378b664f74a16fbc71857de082ca
RonaldEAM <ronaldeam29@gmail.com>
2018-08-07 19:04:22 -0400

    Fix migration values for crypto

--- src/Migrations/Version20180802202905.php
+++ src/Migrations/Version20180802202905.php
@@ -16,7 +16,7 @@ final class Version20180802202905 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE crypto (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, symbol VARCHAR(5) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('INSERT INTO crypto (name, symbol) VALUES (Webchain, WEB)');
+        $this->addSql('INSERT INTO crypto (name, symbol) VALUES (\'Webchain\', \'WEB\')');
     }
 
     public function down(Schema $schema) : void
