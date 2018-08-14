19dd4bd0bea48235d69cb01d8b519fd0432383de
Adel Armand <adel.armand@hotmail.com>
2018-08-14 15:31:52 +0430

    [template] Modify profile view

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,12 +1,4 @@
-import PersonalDataForm from '../components/profile/PersonalDataForm';
-import AccountSettings from '../components/profile/AccountSettings';
-import Tabs from 'bootstrap-vue/es/components';
-
 new Vue({
     el: '#profile',
-    components: {
-        PersonalDataForm,
-        AccountSettings,
-        Tabs,
-    },
+    components: {},
 });
