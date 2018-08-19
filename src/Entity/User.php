0bf0848ca9069c16455d953902dc2b8ff8a208e2
bigshan <segun.adeniji01@gmail.com>
2018-08-19 14:56:38 +0100

    add the profile page url to the top navbar link

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,6 +49,12 @@ class User extends BaseUser
      * @var Profile
      */
     protected $profile;
+    
+    public function getProfile(): ?Profile
+    {
+        return $this->profile;
+    }
+
 
     public function getTempEmail(): ?string
     {
