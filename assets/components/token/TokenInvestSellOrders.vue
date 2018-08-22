cb36020dbc6b397a8b0711046fe398583c034461
bigshan <segun.adeniji01@gmail.com>
2018-08-22 16:48:11 +0100

    correct syntax

--- assets/components/token/TokenInvestSellOrders.vue
+++ assets/components/token/TokenInvestSellOrders.vue
@@ -100,7 +100,6 @@
 </template>
 
 <script>
-import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 export default {
--- assets/components/token/TokenInvestTradeHistory.vue
+++ assets/components/token/TokenInvestTradeHistory.vue
@@ -139,7 +139,6 @@
 </template>
 
 <script>
-import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 export default {
