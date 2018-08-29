8ab235b03cbf170a7a97d050b1ce4d2d238183cf
RonaldEAM <ronaldeam29@gmail.com>
2018-08-29 16:21:03 -0400

    Add edition of token description

--- assets/components/token/TokenIntroductionDescription.vue
+++ assets/components/token/TokenIntroductionDescription.vue
@@ -1,17 +1,24 @@
 <template>
-    <div :class="containerClass">
+    <div>
         <div class="card h-100">
             <div class="card-header">
                 Description
             </div>
             <div class="card-body">
                 <div class="row">
-                    <div class="col">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book.
+                    <div class="col-12">
+                        <font-awesome-icon
+                            v-if="editable"
+                            class="icon float-right"
+                            size="2x"
+                            :icon="icon"
+                            transform="shrink-4 up-1.5"
+                            @click="editDescription"
+                        />
+                        <p v-if="!editingDescription">{{ currentDescription }}</p>
+                        <template v-if="editable">
+                            <textarea class="form-control" v-model="newDescription" v-if="editingDescription"></textarea>
+                        </template>
                     </div>
                 </div>
             </div>
@@ -20,13 +27,73 @@
 </template>
 
 <script>
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import axios from 'axios';
+import Toasted from 'vue-toasted';
+
+library.add(faEdit, faCheck);
+Vue.use(Toasted, {
+    position: 'top-center',
+    duration: 5000,
+});
+
+const HTTP_NO_CONTENT = 204;
+const HTTP_BAD_REQUEST = 400;
+
 export default {
     name: 'TokenIntroductionDescription',
     props: {
-        containerClass: String,
+        description: String,
+        csrfToken: String,
+        updateUrl: String,
+        editable: Boolean,
+    },
+    components: {
+        FontAwesomeIcon,
     },
     data() {
-        return {};
+        return {
+            editingDescription: false,
+            icon: 'edit',
+            currentDescription: this.description,
+            newDescription: this.description,
+        };
     },
+    methods: {
+        editDescription: function() {
+            if (this.icon == 'check')
+                return this.doEditDescription();
+            if (!this.editable)
+                return;
+            this.editingDescription = !this.editingDescription;
+            this.icon = 'check';
+        },
+        doEditDescription: function() {
+            axios.patch(this.updateUrl, {
+                description: this.newDescription,
+                _csrf_token: this.csrfToken,
+            })
+            .then(response => {
+                if (response.status === HTTP_NO_CONTENT) {
+                    this.currentDescription = this.newDescription;
+                }
+            }, error => {
+                if (error.response.status === HTTP_BAD_REQUEST) {
+                    this.$toasted.error(error.response.data[0][0].message);
+                }
+                else {
+                    this.$toasted.error('An error has ocurred, please try again later');
+                }
+            })
+            .then(() => {
+                this.newDescription = this.currentDescription;
+                this.editingDescription = false;
+                this.icon = 'edit';
+            });
+        }
+    }
 };
 </script>
