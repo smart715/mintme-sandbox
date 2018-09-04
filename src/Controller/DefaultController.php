5e7067a57569f0d70976b0f942a1e471aed15ef0
bigshan <segun.adeniji01@gmail.com>
2018-09-04 09:27:11 +0100

    syntax correction

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,23 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Profile;
-use App\Entity\User;
-use App\Form\AddProfileType;
-use App\Form\EditProfileDescriptionType;
-use App\Form\EditProfileType;
-use App\Manager\ProfileManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class DefaultController extends Controller
 {
-     /**
+    /**
      * @Route("/", name="homepage")
      */
     public function index(): Response
@@ -43,88 +33,11 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/profile/{pageUrl}", name="profile")
-     */
-    public function profile(Request $request, ProfileManagerInterface $profileManagerInterface, ?string $pageUrl = null): Response
-    {
-        if (!empty($pageUrl)) {
-            $profile = $profileManagerInterface->getProfileByPageUrl($pageUrl);
-            if (null !== $profile) {
-                 return $this->viewProfile($request, $profile, $profileManagerInterface);
-            } else {
-                return $this->profileNotFoundPage();
-            }
-        }
-        if ($this->getUser()->getProfile())
-            return $this->redirect('/profile/'.$this->getUser()->getProfile()->getPageUrl());
-        
-        return $this->addProfile($request, $profileManagerInterface);
-    }
-    
-    public function viewProfile(Request $request, Profile $profile, ProfileManagerInterface $profileManagerInterface): Response
-    {
-        $form = $this->createForm(EditProfileType::class, $profile);
-        if (!empty($profile->getNameChangedDate()) &&
-            $this->getNumberOfDays($profile->getNameChangedDate()) <= 30 ) {
-            $form->remove('lastname');
-            $form->remove('firstname');
-        }
-        $form->handleRequest($request);
-        if (!$form->isSubmitted() || !$form->isValid())
-            return $this->render('pages/profile_view.html.twig', [
-                'lastName' => $profile->getLastName(),
-                'firstName' => $profile->getFirstName(),
-                'city' => $profile->getCity(),
-                'country' => $profile->getCountry(),
-                'description' => $profile->getDescription(),
-                'canedit' => ($profile === $this->getUser()->getProfile()) ? true : false,
-                'showeditFormFirst' => (empty($profile->getCity()) && empty($profile->getCountry()) && empty($profile->getDescription())) ? true : false,
-                'form' =>  $form->createView(),
-            ]);
-        
-        $profile->setPageUrl($profileManagerInterface->generatePageUrl($profile));
-        $today = new \DateTime();
-        $today->format('Y-m-d H:i:s');
-        $profile->setNameChangedDate($today);
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($profile);
-        $entityManager->flush();
-        return $this->redirect('/profile/'.$profile->getPageUrl());
-    }
-    
-    public function profileNotFoundPage(): Response
-    {
-        return $this->render('pages/profile_404.html.twig', []);
-    }
-    
-    public function addProfile(Request $request, ProfileManagerInterface $profileManagerInterface): Response
-    {
-        $user = $this->getUser();
-        $profile  = new Profile($user);
-        $form = $this->createForm(AddProfileType::class, $profile);
-        $form->handleRequest($request);
-        
-        if (!$form->isSubmitted() || !$form->isValid())
-            return $this->render('pages/profile.html.twig', [
-                'form' => $form->createView(),
-            ]);
-        
-        $profile->setPageUrl($profileManagerInterface->generatePageUrl($profile));
-        $today = new \DateTime();
-        $today->format('Y-m-d H:i:s');
-        $profile->setNameChangedDate($today);
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($profile);
-        $entityManager->flush();
-        return $this->redirect('/profile/'.$profile->getPageUrl());
-    }
-        
-    /**
      * @Route("/token/{name}/{tab}", name="token")
      */
-    public function token(?string $name = null, ?string $tab = null): Response
+    public function token(?String $name = null, ?String $tab = null): Response
     {
-        // FIXME: This data is for view test only.
+        // FIXME: This is for view test only.
         $tokenName = $name;
         $action = 'invest';
         $tab = strtolower(strval($tab));
@@ -145,13 +58,5 @@ class DefaultController extends Controller
             'tokenName' => $tokenName,
             'action' => $action,
         ]);
-    }
-       
-    private function getNumberOfDays(\DateTime $from): int
-    {
-        $today = new \DateTime();
-        $today->format('Y-m-d H:i:s');
-        $interval = $from->diff($today);
-        return intval($interval->format('%R%a'));
     }
 }
---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Profile;
+use App\Entity\User;
+use App\Form\AddProfileType;
+use App\Form\EditProfileDescriptionType;
+use App\Form\EditProfileType;
+use App\Manager\ProfileManagerInterface;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProfileController extends Controller
+{
+    
+    /**
+     * @Route("/profile/{pageUrl}", name="profile")
+     */
+    public function profile(Request $request, ProfileManagerInterface $profileManager, ?string $pageUrl = null): Response
+    {
+        if (!empty($pageUrl)) {
+            $profile = $profileManager->getProfileByPageUrl($pageUrl);
+
+            if (null === $profile) {
+                 throw new NotFoundHttpException();
+            }
+
+            return $this->viewProfile($request, $profile, $profileManager);
+        }
+
+        return $this->addProfile($request, $profileManager);
+    }
+    
+    public function viewProfile(Request $request, Profile $profile, ProfileManagerInterface $profileManager): Response
+    {
+        $form = $this->createForm(EditProfileType::class, $profile);
+        $form->handleRequest($request);
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('pages/profile_view.html.twig', [
+                'lastName' => $profile->getLastName(),
+                'firstName' => $profile->getFirstName(),
+                'city' => $profile->getCity(),
+                'country' => $profile->getCountry(),
+                'description' => $profile->getDescription(),
+                'canEdit' => ($profile === $this->getUser()->getProfile()) ? true : false,
+                'showEditFormFirst' => (empty($profile->getCity()) && empty($profile->getCountry()) && empty($profile->getDescription())) ? true : false,
+                'form' =>  $form->createView(),
+            ]);
+        }
+        $profile->setPageUrl($profileManager->generatePageUrl($profile));
+        $now = new DateTime();
+        $profile->setNameChangedDate($now);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($profile);
+        $entityManager->flush();
+        return $this->redirect('/profile/'.$profile->getPageUrl());
+    }
+    
+    public function addProfile(Request $request, ProfileManagerInterface $profileManager): Response
+    {
+        $user = $this->getUser();
+        $profile  = new Profile($user);
+        $form = $this->createForm(AddProfileType::class, $profile);
+        $form->handleRequest($request);
+        
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->render('pages/profile.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+        $profile->setPageUrl($profileManager->generatePageUrl($profile));
+        $now = new DateTime();
+        $profile->setNameChangedDate($now);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($profile);
+        $entityManager->flush();
+        return $this->redirect('/profile/'.$profile->getPageUrl());
+    }
+}
