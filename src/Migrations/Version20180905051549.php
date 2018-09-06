ab77f0f570f89e435df1272f686aa1863aebd058
bigshan <segun.adeniji01@gmail.com>
2018-09-06 09:31:06 +0100

    update migration file

--- src/Migrations/Version20180905051549.php
+++ src/Migrations/Version20180905051549.php
@@ -15,7 +15,7 @@ final class Version20180905051549 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE profile ADD referencer_id INT DEFAULT NULL, ADD referral_code VARCHAR(255) NOT NULL, DROP page_url');
+        $this->addSql('ALTER TABLE profile ADD referencer_id INT DEFAULT NULL, ADD referral_code VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20180905051549 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE profile ADD page_url VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci, DROP referencer_id, DROP referral_code');
+        $this->addSql('ALTER TABLE profile DROP referencer_id, DROP referral_code');
     }
 }
