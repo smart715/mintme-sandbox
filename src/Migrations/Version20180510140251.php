4f97f20d504cf23be6c1d7f80a0bee10c6a06190
bigshan <segun.adeniji01@gmail.com>
2018-09-16 20:43:43 +0100

    add if not exists to table google_authenticator_entry

--- src/Migrations/Version20180510140251.php
+++ src/Migrations/Version20180510140251.php
@@ -16,7 +16,7 @@ final class Version20180510140251 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE `google_authenticator_entry` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, secret VARCHAR(255) DEFAULT NULL, backup_codes LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_728961A7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE if not exists `google_authenticator_entry` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, secret VARCHAR(255) DEFAULT NULL, backup_codes LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_728961A7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
