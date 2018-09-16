39950eeb8e40cc6bd68b70e5718dda4c8d0028e1
bigshan <segun.adeniji01@gmail.com>
2018-09-16 20:27:11 +0100

    fixed migration error

--- src/Migrations/Version20180510140251.php
+++ src/Migrations/Version20180510140251.php
@@ -17,7 +17,6 @@ final class Version20180510140251 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE `google_authenticator_entry` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, secret VARCHAR(255) DEFAULT NULL, backup_codes LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_728961A7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE `google_authenticator_entry` ADD CONSTRAINT FK_728961A7A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
     }
 
     public function down(Schema $schema): void
