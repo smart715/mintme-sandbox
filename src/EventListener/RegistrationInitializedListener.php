7457a644c53878a65a268bf6321b161eb660944a
bigshan <segun.adeniji01@gmail.com>
2018-10-14 19:40:22 +0100

    prevent authorized user to acces lon/register page

---
+++ src/EventListener/RegistrationInitializedListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventListener;
+
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+/**
+ * Listener of the event "visitor is about to register" (he navigates register page).
+ * The purpose is to prevent already authenticated users from seeing registration form,
+ * and redirect them to profile instead.
+ */
+class RegistrationInitializedListener
+{
+    /** @var UrlGeneratorInterface */
+    private $router;
+    
+    /** @var AuthorizationCheckerInterface */
+    private $authorizationChecker;
+
+    public function __construct(
+        UrlGeneratorInterface $router,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
+        $this->router = $router;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function onFosuserRegistrationInitialize(GetResponseUserEvent $event): void
+    {
+        $this->redirectToReferralIfAuthenticated($event);
+    }
+    
+    private function redirectToReferralIfAuthenticated(GetResponseUserEvent $event): void
+    {
+        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return;
+        }
+
+        $event->setResponse(new RedirectResponse(
+            $this->router->generate('referral')
+        ));
+    }
+}
