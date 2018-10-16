c33bbf3d1151d7c94ce43f411cf4823f2319afad
Adel Armand <adel.armand@hotmail.com>
2018-10-16 19:02:19 +0330

    [TokenChart] Implement front-end

--- assets/components/token/trade/TokenTradeChart.vue
+++ assets/components/token/trade/TokenTradeChart.vue
@@ -3,9 +3,9 @@
         <div class="card h-100">
             <div class="card-header"></div>
             <div class="card-body p-2">
-                <div class="text-center">
+                <div class="small text-center">
                     <div class="pt-2">
-                        Last price: 0.000000001WEB
+                        Last price: {{ marketStatus.volume }}
                         <font-awesome-icon
                             icon="question"
                             class="ml-1 mb-1 bg-primary text-white
@@ -22,7 +22,7 @@
                                            rounded-circle square blue-question"
                                 />
                             </div>
-                            <div>+2.002%</div>
+                            <div>{{ marketStatus.change }}</div>
                         </div>
                         <div class="d-inline-block px-2">
                             <div>
@@ -33,12 +33,12 @@
                                            rounded-circle square blue-question"
                                 />
                             </div>
-                            <div>52555.25354 Tokens</div>
+                            <div>{{ marketStatus.volume }} Tokens</div>
                         </div>
                     </div>
                 </div>
                 <div class="pt-3">
-                    [Volume]WEB(+2%)
+                    [Volume]WEB({{ marketStatus.change }}%)
                 </div>
                 <line-chart
                     :data="chartData"
@@ -63,14 +63,14 @@ export default {
     data() {
         return {
             chartData: {
-                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                labels: [],
                 datasets: [
                     {
                         borderWidth: 0,
                         borderColor: 'transparent',
                         backgroundColor: '#8ec63f',
                         radius: 0,
-                        data: [0, 35, 21, 69, 53, 87, 15, 10, 19],
+                        data: [],
                         lineTension: 0,
                     },
                 ],
@@ -110,16 +110,35 @@ export default {
                     display: false,
                 },
             },
+            chartXAxesPoints: 10,
             wsClient: null,
             wsResult: {},
+            marketStatus: {
+                volume: 0,
+                last: 0,
+                change: 0,
+            },
         };
     },
     computed: {
         market: function() {
             return JSON.parse(this.marketName);
         },
+        chartInitValues: function() {
+            let values = [];
+            let i = 0;
+            while (i < this.chartXAxesPoints) {
+                values.push(0);
+                ++i;
+            }
+            return values;
+        },
     },
     mounted() {
+        // Init values cause chart stick to xAxes initially.
+        this.chartData.labels = this.chartInitValues;
+        this.chartData.datasets[0].data = this.chartInitValues;
+
         if (this.websocketUrl) {
             this.wsClient = new W3CWebSocket(this.websocketUrl);
             this.wsClient.onmessage = (result) => {
@@ -136,8 +155,43 @@ export default {
             };
         }
     },
+    methods: {
+        updateMarketData: function(marketData) {
+            if (!marketData.params) {
+                return;
+            }
+
+            const marketInfo = marketData.params[1];
+            const marketOpenPrice = parseFloat(marketInfo.open);
+            const marketLastPrice = parseFloat(marketInfo.last);
+            const makretVolume = parseFloat(marketInfo.volume);
+            const priceDiff = marketLastPrice - marketOpenPrice;
+            const changePercentage = marketOpenPrice ? priceDiff * 100 / marketOpenPrice : 0;
+
+            this.marketStatus = {
+                change: changePercentage.toFixed(2),
+                last: marketLastPrice.toFixed(2),
+                volume: makretVolume.toFixed(2),
+            };
+
+            let data = this.chartData.datasets[0].data;
+            data.push(makretVolume);
+            if (data.length > this.chartXAxesPoints) {
+                data = data.slice(1);
+            }
+            this.chartData.datasets[0].data = data;
+        },
+    },
     components: {
         LineChart,
+    },
+    watch: {
+        wsResult: {
+            handler(value) {
+                this.updateMarketData(value);
+            },
+            deep: true,
+        },
     },
 };
 </script>
