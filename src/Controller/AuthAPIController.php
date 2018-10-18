b1ef6202a7c6f8914c4d18edd8987905a117c453
denlev <levadnyuk66@gmail.com>
2018-10-18 15:41:44 +0300

    [php] Removed apache headers function

--- src/Controller/AuthAPIController.php
+++ src/Controller/AuthAPIController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class AuthAPIController
 {
     public function authUser(): JsonResponse {
-        var_dump(apache_request_headers());
         return new JsonResponse(
             [
                 "code" => 0,
