1cde544e5657cfea396659fe62f089dc3bcbc93d
bigshan <segun.adeniji01@gmail.com>
2018-10-26 22:26:21 +0100

    syntax corrected

--- src/Validator/Constraints/UserPasswordValidator.php
+++ src/Validator/Constraints/UserPasswordValidator.php
@@ -7,6 +7,10 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class UserPasswordValidator extends ConstraintValidator
 {
+    /**
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+     * @param string $value
+     */
     public function validate($value, Constraint $constraint): void
     {
         if (!empty($value) && !preg_match('/(?=.*[\p{Lu}])(?=.*[\p{Ll}])(?=.*[\p{N}]).{8,}/', $value)) {
