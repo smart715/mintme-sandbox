9196d50ae0f658c7823e06602125981ed53a096b
Victor <fedorenko22116@gmail.com>
2018-10-31 13:02:17 +0200

    [php]Updated hash for user db

--- src/Migrations/Version20181031111642.php
+++ src/Migrations/Version20181031111642.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181031090642 extends AbstractMigration
+final class Version20181031111642 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user DROP hash');
+        $this->addSql('ALTER TABLE user ADD hash VARCHAR(255)');
     }
 
     public function down(Schema $schema) : void
