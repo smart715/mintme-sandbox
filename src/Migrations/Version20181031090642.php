29971492d9c7c96218c87aa01f0f307e7359caa6
Victor <fedorenko22116@gmail.com>
2018-10-31 12:39:10 +0200

    [php]Removed hash from db

--- src/Migrations/Version20181031090642.php
+++ src/Migrations/Version20181031090642.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181030090642 extends AbstractMigration
+final class Version20181031090642 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
