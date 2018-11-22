c0c1809aa026fdb88ba284914d06fdca7bfc482f
denlev <levadnyuk66@gmail.com>
2018-11-22 16:26:16 +0200

    [php] Added constructor for UserManager

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -4,15 +4,23 @@ namespace App\Manager;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface;
+use FOS\UserBundle\Util\CanonicalFieldsUpdater;
+use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 class UserManager extends \FOS\UserBundle\Doctrine\UserManager implements UserManagerInterface
 {
     /** @var UserRepository */
     private $userRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        PasswordUpdaterInterface $passwordUpdater,
+        CanonicalFieldsUpdater $fieldsUpdater,
+        ObjectManager $objectManager
+    ) {
+        parent::__construct($passwordUpdater, $fieldsUpdater, $objectManager, $this->getClass());
         $this->userRepository = $entityManager->getRepository(User::class);
     }
 
