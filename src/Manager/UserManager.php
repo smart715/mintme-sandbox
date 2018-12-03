3ca767b28cb26f7a1a06b00a499af5749092b953
denlev <levadnyuk66@gmail.com>
2018-12-03 16:22:12 +0200

    [php]Fixed UserManager after merge

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -4,25 +4,9 @@ namespace App\Manager;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\EntityManagerInterface;
-use FOS\UserBundle\Util\CanonicalFieldsUpdater;
-use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 class UserManager extends \FOS\UserBundle\Doctrine\UserManager implements UserManagerInterface
 {
-    /** @var UserRepository */
-    private $userRepository;
-
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        PasswordUpdaterInterface $passwordUpdater,
-        CanonicalFieldsUpdater $fieldsUpdater,
-        ObjectManager $objectManager
-    ) {
-        parent::__construct($passwordUpdater, $fieldsUpdater, $objectManager, $this->getClass());
-        $this->userRepository = $entityManager->getRepository(User::class);
-    }
 
     public function find(int $id): ?User
     {
@@ -36,11 +20,11 @@ class UserManager extends \FOS\UserBundle\Doctrine\UserManager implements UserMa
 
     public function findByEmail(string $email): ?User
     {
-        return $this->userRepository->findByEmail($email);
+        return $this->getRepository()->findByEmail($email);
     }
 
     public function findByIds(array $userIds): array
     {
-        return $this->userRepository->findByIds($userIds);
+        return $this->getRepository()->findByIds($userIds);
     }
 }
