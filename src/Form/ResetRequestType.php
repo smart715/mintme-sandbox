c74cc796ee299bc5eed0dea8cf68d7ca0a854bab
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-01-16 16:10:26 +0000

    add EmailType to password reset form #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Email;
 
