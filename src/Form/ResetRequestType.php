10b203c50460c6d81f1eb6421fa87661df873caf
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-01-16 16:00:51 +0000

    backend message in reset password page #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -6,12 +6,17 @@ use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
 
 class ResetRequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('username', EmailType::class, [
+                'label' => 'Email', 'attr' => array('pattern' => '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'),
+                'constraints' => [ new Email(['message' => 'Invalid email address.', 'checkMX' => true]) ],
+            ])
             ->add('recaptcha', EWZRecaptchaType::class, [
                 'attr' => [
                     'options' => [
