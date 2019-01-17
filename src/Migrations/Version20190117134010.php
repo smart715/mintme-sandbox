8e7ed7ede1c13040daa73b5575e5860b9ea360ba
osama <osamaesmail2@gmail.com>
2019-01-17 18:48:07 +0200

    update migration file name to old one

--- src/Migrations/Version20190117134010.php
+++ src/Migrations/Version20190117134010.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190117161055 extends AbstractMigration
+final class Version20190117134010 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
