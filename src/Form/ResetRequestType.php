1f148b4babfeed38c0f37d6a0a60f9cb20d04b40
adam <ahmed.elaaser@outlook.com>
2019-01-23 17:29:20 +0000

    reset to master #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -5,19 +5,13 @@ namespace App\Form;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Email;
 
 class ResetRequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'label' => false, 'attr' => array('pattern' => '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$', 'autofocus' => 'true'),
-                'constraints' => [ new Email(['message' => 'Invalid email address.', 'checkMX' => true]) ],
-            ])
             ->add('recaptcha', EWZRecaptchaType::class, [
                 'attr' => [
                     'options' => [
