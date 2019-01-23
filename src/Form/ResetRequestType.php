04818eff390f2aad9d4d9447ff284c6ccaff38b9
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2019-01-23 18:09:47 +0000

    backend validation fix #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -4,14 +4,22 @@ namespace App\Form;
 
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
 
 class ResetRequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('username', EmailType::class, [
+                'label' => 'Email', 'attr' => array('pattern' => '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'),
+                'constraints' => [new Email(['message' => 'Invalid email address.', 'checkMX' => true]) ],
+            ])
+        
             ->add('recaptcha', EWZRecaptchaType::class, [
                 'attr' => [
                     'options' => [
