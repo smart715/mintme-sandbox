c3b5afe5e1a9ef792fd9f86a3c995737661b001d
adam <ahmed.elaaser@outlook.com>
2019-02-06 04:20:14 +0000

    rewrite validation code #4725

--- src/Form/Model/UsernameModel.php
+++ src/Form/Model/UsernameModel.php
@@ -2,7 +2,6 @@
 
 namespace App\Form\Model;
 
-use App\Validator\Constraints\UserEmail;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class UsernameModel
@@ -14,22 +13,6 @@ class UsernameModel
      *     message = "Invalid email address.",
      *     checkMX = true
      * )
-     * @UserEmail()
      */
     private $username;
-
-    public function __construct(string $username)
-    {
-        $this->username = $username;
-    }
-
-    public function getUsername(): string
-    {
-        return (string)$this->username;
-    }
-
-    public function setUsername(?string $username): void
-    {
-        $this->username = $username;
-    }
 }
