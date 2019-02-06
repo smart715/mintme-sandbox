e5df6dda99a37b353904bbac2a626ef298cbfee9
adam <ahmed.elaaser@outlook.com>
2019-02-06 03:38:14 +0000

    use email instant of username #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -2,21 +2,19 @@
 
 namespace App\Form;
 
-use App\Form\Model\UsernameModel;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ResetRequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', EmailType::class, [
+            ->add('email', EmailType::class, [
                 'label' => false,
             ])
     
@@ -34,10 +32,5 @@ class ResetRequestType extends AbstractType
                 'error_bubbling' => true,
             ])
         ;
-    }
-    
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefault('username', UsernameModel::class);
     }
 }
