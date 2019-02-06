074cdd6b3be02e766f7302a3fde0afe47f7a4f75
adam <ahmed.elaaser@outlook.com>
2019-02-06 01:33:04 +0000

    add EmailModel for backend validation #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -2,16 +2,20 @@
 
 namespace App\Form;
 
+use App\Form\Model\EmailModel;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ResetRequestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email', EmailType::class, [ 'label' => 'Email:' ])
             ->add('recaptcha', EWZRecaptchaType::class, [
                 'attr' => [
                     'options' => [
@@ -26,5 +30,10 @@ class ResetRequestType extends AbstractType
                 'error_bubbling' => true,
             ])
         ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('data_class', EmailModel::class);
     }
 }
