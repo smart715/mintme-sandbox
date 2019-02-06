e3d4f13627ea3c348f0dda388611e700603bf448
adam <ahmed.elaaser@outlook.com>
2019-02-06 02:55:07 +0000

    add SubmitType extension #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -7,6 +7,7 @@ use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
