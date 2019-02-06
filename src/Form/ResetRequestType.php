810538467760755e34e1a4c07efd85ac1ee0b15d
adam <ahmed.elaaser@outlook.com>
2019-02-06 05:09:49 +0000

    use Assert #4725

--- src/Form/ResetRequestType.php
+++ src/Form/ResetRequestType.php
@@ -2,24 +2,27 @@
 
 namespace App\Form;
 
-use App\Form\Model\UsernameModel;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ResetRequestType extends AbstractType
 {
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message = "Invalid email address.",
+     *     checkMX = true
+     * )
+     * @var string
+     */
+     protected $username;
+     
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', EmailType::class, [
-                'label' => false,
-            ])
-    
             ->add('recaptcha', EWZRecaptchaType::class, [
                 'attr' => [
                     'options' => [
@@ -34,5 +37,11 @@ class ResetRequestType extends AbstractType
                 'error_bubbling' => true,
             ])
         ;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return parent::setUsername($username);
     }
 }
