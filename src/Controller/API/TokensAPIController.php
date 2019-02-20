2bc9f7b740358730a8ec4559c19cbd5d7915f443
osama <osamaesmail2@gmail.com>
2019-02-20 10:47:05 +0200

    remove unused use statments

--- src/Controller/API/TokensAPIController.php
+++ src/Controller/API/TokensAPIController.php
@@ -9,7 +9,6 @@ use App\Form\TokenType;
 use App\Manager\CryptoManagerInterface;
 use App\Manager\TokenManagerInterface;
 use App\Verify\WebsiteVerifier;
-use App\Verify\WebsiteVerifierInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
@@ -19,7 +18,6 @@ use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\Constraints\Url;
 use Symfony\Component\Validator\Validation;
 
