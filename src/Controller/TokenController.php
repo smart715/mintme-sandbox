d65821ae55b81c2430acf29aad4ca594a0018dfa
Khaled Alam <khaledalam.net@gmail.com>
2019-02-24 13:46:04 +0000

    make syntax_check. Refs #4856

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -191,7 +191,7 @@ class TokenController extends AbstractController
         return $response;
     }
 
-    public function normalizeTokenName(?string $name) :string
+    public function normalizeTokenName(?string $name): string
     {
         $name = $name ?? '';
         $name = trim(strtolower($name));
