3e6378caad04cc08b3244f4e7cacdd9f089bfe8e
denlev <levadnyuk66@gmail.com>
2019-05-01 23:51:32 +0300

    [php]Removed codeCOverage

--- src/Command/MigrateMarketsCommand.php
+++ src/Command/MigrateMarketsCommand.php
@@ -8,9 +8,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * @codeCoverageIgnore
- */
 class MigrateMarketsCommand extends Command
 {
     /** @var MarketFactoryInterface */
