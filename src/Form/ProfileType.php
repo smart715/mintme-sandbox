baba283b8bde40086a97f88940d977969a579c43
Khaled Alam <khaledalam.net@gmail.com>
2019-06-02 17:00:39 +0200

    show all profile inputs on creation. #5154

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /** @codeCoverageIgnore  */
-class EditProfileType extends AbstractType
+class ProfileType extends AbstractType
 {
     /** @var NameTransformer  */
     private $nameTransformer;
