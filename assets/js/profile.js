ed071b4c7bee8f1d84170c3b02cecedb892b2e4d
Ninjo <khaledalam.net@gmail.com>
2019-06-07 02:13:49 +0000

    correct city regex. #5151

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,7 +1,7 @@
 import LimitedTextarea from './components/LimitedTextarea.vue';
 import {minLength, helpers} from 'vuelidate/lib/validators';
 const names = helpers.regex('names', /^[A-Za-zÁ-Źá-ź]+[A-Za-zÁ-Źá-ź\s'‘’`´-]*$/u);
-const city = helpers.regex('city', /^[A-Za-z\s-]+$/u);
+const city = helpers.regex('city', /^[A-Za-zÁ-Źá-ź\s-]+$/u);
 
 new Vue({
     el: '#profile',
