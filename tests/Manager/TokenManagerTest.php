5a45218e24050eb3c815b97796d05ca869876489
uosama <osamaesmail2@gmail.com>
2019-07-09 15:57:02 +0000

    Issue #4817

--- tests/Manager/TokenManagerTest.php
+++ tests/Manager/TokenManagerTest.php
@@ -4,16 +4,21 @@ namespace App\Tests\Manager;
 
 use App\Entity\Crypto;
 use App\Entity\Profile;
+use App\Entity\Token\LockIn;
 use App\Entity\Token\Token;
+use App\Entity\User;
+use App\Exchange\Balance\Model\BalanceResult;
 use App\Exchange\Config\Config;
 use App\Manager\CryptoManagerInterface;
 use App\Manager\TokenManager;
 use App\Repository\TokenRepository;
 use App\Utils\Fetcher\ProfileFetcherInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Money\Money;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class TokenManagerTest extends TestCase
 {
@@ -98,10 +103,161 @@ class TokenManagerTest extends TestCase
         $this->assertEquals(null, $tokenManager->getOwnToken());
     }
 
-    /** @return MockObject|TokenStorageInterface */
-    private function mockTokenStorage(): TokenStorageInterface
+    /** @dataProvider findByHiddenNameDataProvider */
+    public function testFindByHiddenName(string $expected, string $origin): void
     {
-        return $this->createMock(TokenStorageInterface::class);
+        $repo = $this->createMock(TokenRepository::class);
+        $repo->expects($this->once())
+            ->method('find')
+            ->with($expected)
+            ->willReturn($this->createMock(Token::class));
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->method('getRepository')->willReturn($repo);
+
+        $tokenManager = new TokenManager(
+            $em,
+            $this->createMock(ProfileFetcherInterface::class),
+            $this->mockTokenStorage(),
+            $this->mockCryptoManager([]),
+            $this->mockConfig(1)
+        );
+
+        $tokenManager->findByHiddenName($origin);
+    }
+
+    public function findByHiddenNameDataProvider(): array
+    {
+        return [
+            [122, '123'],
+            [122, 'qwe123'],
+            [122, 'qwe123qwe'],
+            [-1, 'qwe'],
+        ];
+    }
+
+    public function testIsExisted(): void
+    {
+        $fooTok = $this->mockToken(' foo   23-fg  fd  ');
+        $barTok = $this->mockToken('bar');
+
+        /** @var MockObject|TokenRepository $tokenRepository */
+        $tokenRepository = $this->createMock(TokenRepository::class);
+        $tokenRepository->method('findByName')->with('FOO-23-FG-FD')->willReturn($barTok);
+
+        /** @var MockObject|EntityManagerInterface $entityManager */
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->method('getRepository')->willReturn($tokenRepository);
+
+        $tokenManager = new TokenManager(
+            $entityManager,
+            $this->createMock(ProfileFetcherInterface::class),
+            $this->mockTokenStorage(),
+            $this->mockCryptoManager([]),
+            $this->mockConfig(0)
+        );
+
+        $this->assertTrue($tokenManager->isExisted($fooTok));
+    }
+
+    /** @dataProvider getRealBalanceProvider */
+    public function testGetRealBalance(
+        Token $token,
+        bool $hasProfile,
+        int $eAvailable,
+        int $eFreeze,
+        int $eReferral
+    ): void {
+        $profile = $this->createMock(Profile::class);
+        $profile->method('getToken')->willReturn($token);
+        $profile->method('getUser')->willReturn($token->getProfile()->getUser());
+
+        /** @var MockObject|ProfileFetcherInterface $profileFetcher */
+        $profileFetcher = $this->createMock(ProfileFetcherInterface::class);
+        $profileFetcher->method('fetchProfile')->willReturn($hasProfile ? $profile: null);
+
+        $tokenManager = new TokenManager(
+            $this->createMock(EntityManagerInterface::class),
+            $profileFetcher,
+            $this->mockTokenStorage($token->getProfile()->getUser()),
+            $this->mockCryptoManager([]),
+            $this->mockConfig(0)
+        );
+
+        $br = $this->createMock(BalanceResult::class);
+        $br->method('getReferral')->willReturn(Money::USD(1));
+        $br->method('getFreeze')->willReturn(Money::USD(1));
+        $br->method('getAvailable')->willReturn(Money::USD(1));
+
+        $res = $tokenManager->getRealBalance($token, $br);
+
+        $this->assertEquals($eAvailable, $res->getAvailable()->getAmount());
+        $this->assertEquals($eFreeze, $res->getFreeze()->getAmount());
+        $this->assertEquals($eReferral, $res->getReferral()->getAmount());
+    }
+
+    public function getRealBalanceProvider(): array
+    {
+        return [
+            [$this->mockToken('foo'), true, 1, 1, 1],
+            [$this->mockToken('foo', $this->mockLockIn(1)), true, 0, 2, 1],
+            [$this->mockToken('foo', $this->mockLockIn(1), $this->createMock(User::class)), true, 0, 2, 1],
+        ];
+    }
+
+    public function testFindAllPredefined(): void
+    {
+        $tokenManager = new TokenManager(
+            $this->createMock(EntityManagerInterface::class),
+            $this->createMock(ProfileFetcherInterface::class),
+            $this->createMock(TokenStorageInterface::class),
+            $this->mockCryptoManager([
+                $this->mockCrypto('foo'),
+                $this->mockCrypto('bar'),
+            ]),
+            $this->mockConfig(0)
+        );
+
+        $toks = $tokenManager->findAllPredefined();
+
+        $this->assertCount(2, $toks);
+        $this->assertEquals(['foo', 'bar'], [
+            $toks[0]->getSymbol(),
+            $toks[1]->getSymbol(),
+        ]);
+    }
+
+    private function mockToken(string $name, ?LockIn $lockIn = null, ?User $user = null): Token
+    {
+        $profile = $this->createMock(Profile::class);
+        $profile->method('getUser')->willReturn($user ?? $this->createMock(User::class));
+
+        $tok = $this->createMock(Token::class);
+        $tok->method('getName')->willReturn($name);
+        $tok->method('getLockIn')->willReturn($lockIn);
+        $tok->method('getProfile')->willReturn($profile);
+
+        return $tok;
+    }
+
+    private function mockLockIn(int $frozen): LockIn
+    {
+        $lockIn = $this->createMock(LockIn::class);
+        $lockIn->method('getFrozenAmount')->willReturn(Money::USD($frozen));
+
+        return $lockIn;
+    }
+
+    /** @return MockObject|TokenStorageInterface */
+    private function mockTokenStorage(?User $user = null): TokenStorageInterface
+    {
+        $token = $this->createMock(TokenInterface::class);
+        $token->method('getUser')->willReturn($user);
+
+        $storage = $this->createMock(TokenStorageInterface::class);
+        $storage->method('getToken')->willReturn($token);
+
+        return $storage;
     }
 
     /**
