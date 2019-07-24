ff04400fba4db013040534c123e8474f1e020887
denlev <levadnyuk66@gmail.com>
2019-07-24 13:33:28 +0300

    [php]Created show one kb action and added template

--- src/Controller/KnowledgeBaseController.php
+++ src/Controller/KnowledgeBaseController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\KnowledgeBase\KnowledgeBase;
 use App\Manager\KnowledgeBaseManagerInterface;
 use FOS\RestBundle\Controller\Annotations\Route;
+use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 
 class KnowledgeBaseController extends Controller
@@ -12,10 +12,26 @@ class KnowledgeBaseController extends Controller
     /**
      * @Route(path="/kb", name="kb")
      */
-    public function showAll(KnowledgeBaseManagerInterface $knowledgeBaseManager): Response
+    public function showAll(KnowledgeBaseManagerInterface $kbManager): Response
     {
         return $this->render('pages/knowledge_base.html.twig', [
-            'articles' => $knowledgeBaseManager->getAll(),
+            'articles' => $kbManager->getAll(),
+        ]);
+    }
+
+    /**
+     * @Route(path="/kb/{url}", name="kb_show", methods={"GET"})
+     */
+    public function show(string $url, KnowledgeBaseManagerInterface $kbManager): Response
+    {
+        $article = $kbManager->getByUrl($url);
+
+        if (!$article) {
+            throw new InvalidArgumentException();
+        }
+
+        return $this->render('pages/knowledge_base_show.html.twig', [
+           'article' => $article,
         ]);
     }
 }
