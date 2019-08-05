f3a5a5e31c30a914b4ef2df78b2940a09b6d87a1
alexsobolenko <alexsobolenko@gmail.com>
2019-08-05 10:58:58 +0300

    [BbcodeViewSpec.js] fixed unit tests

--- assets/tests/components/BbcodeViewSpec.js
+++ assets/tests/components/BbcodeViewSpec.js
@@ -4,25 +4,25 @@ import BbcodeView from '../../js/components/bbcode/BbcodeView';
 describe('BbcodeView', () => {
     it('parse bbcode', () => {
         const wrapper = mount(BbcodeView, {
-             propsData: {description: '[h1]Lorem ipsum.[/h1]'},
+             propsData: {value: '[h1]Lorem ipsum.[/h1]'},
         });
 
-        expect(wrapper.vm.parsedDescription).to.equal('<h1>Lorem ipsum.</h1>');
+        expect(wrapper.vm.parsedValue).to.equal('<h1>Lorem ipsum.</h1>');
     });
 
     it('parse bbcode image', () => {
         const wrapper = mount(BbcodeView, {
-             propsData: {description: '[img]foo[/img]'},
+             propsData: {value: '[img]foo[/img]'},
         });
 
-        expect(wrapper.vm.parsedDescription).to.equal('<img style="max-width: 100%;" src="foo"/>');
+        expect(wrapper.vm.parsedValue).to.equal('<img style="max-width: 100%;" src="foo"/>');
     });
 
     it('parse bbcode link', () => {
         const wrapper = mount(BbcodeView, {
-             propsData: {description: '[url=foo]bar[/url]'},
+             propsData: {value: '[url=foo]bar[/url]'},
         });
 
-        expect(wrapper.vm.parsedDescription).to.equal('<a rel="nofollow" target="_blank" href="https://foo">bar</a>');
+        expect(wrapper.vm.parsedValue).to.equal('<a rel="nofollow" target="_blank" href="https://foo">bar</a>');
     });
 });
