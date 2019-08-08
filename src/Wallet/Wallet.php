4c5105e7efa3ee168670b24c3be79daf7852042e
Osama Esmail <osamaesmail2@gmail.com>
2019-08-08 14:54:55 +0200

    fix withdraw BTC

--- src/Wallet/Wallet.php
+++ src/Wallet/Wallet.php
@@ -107,7 +107,7 @@ class Wallet implements WalletInterface
         if ($tradable instanceof Crypto) {
             $fee = $tradable->getFee();
             $crypto = $tradable;
-            $token = Token::getFromSymbol(Token::WEB_SYMBOL);
+            $token = Token::getFromSymbol($tradable->getSymbol());
         } else {
             $fee = new Money('0', new Currency(MoneyWrapper::TOK_SYMBOL));
             $crypto = $this->cryptoManager->findBySymbol(Token::WEB_SYMBOL);
