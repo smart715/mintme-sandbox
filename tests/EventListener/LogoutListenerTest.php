ac53ceff0689b1fe07c46ad8d1d51481b614f966
bigshan <segun.adeniji01@gmail.com>
2019-08-12 00:59:38 +0100

    fixed syntax

--- tests/EventListener/LogoutListenerTest.php
+++ tests/EventListener/LogoutListenerTest.php
@@ -1,27 +1,27 @@
 <?php declare(strict_types = 1);
 
 namespace App\Tests\EventListener;
+
 use App\EventListener\LogoutListener;
 use App\Logger\UserActionLogger;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\HttpFoundation\ParameterBag;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 
 class LogoutListenerTest extends TestCase
 {
     public function testOnLogout(): void
-
     {
         $checker = $this->createMock(AuthorizationCheckerInterface::class);
         $logger = $this->createMock(UserActionLogger::class);
         $session = $this->createMock(SessionInterface::class);
 
-        $checker->method('isGranted')->willReturn(false); 
+        $checker->method('isGranted')->willReturn(false);
 
         $listener = new LogoutListener(
             $logger,
@@ -36,7 +36,6 @@ class LogoutListenerTest extends TestCase
 
         $event->method('getToken')->willReturn($this->createMock(TokenInterface::class));
         $event->method('getRequest')->willReturn($request);
-        $event->method('getResponse')->willReturn($response);
         $listener->logout($event);
     }
 }
