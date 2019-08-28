de9811a112ab61c9c82bde5397d871ef73eeab5e
TheRadly <alex5011999@gmail.com>
2019-08-28 15:46:37 +0300

    fix merge conflict

--- assets/js/token_creation.js
+++ assets/js/token_creation.js
@@ -1,6 +1,6 @@
 import Modal from './components/modal/Modal';
 
-import {minLength, maxLength, helpers} from 'vuelidate/lib/validators';
+import {required, minLength, maxLength, helpers} from 'vuelidate/lib/validators';
 
 const tokenContain = helpers.regex('names', /^[a-zA-Z0-9\s-]*$/u);
 
@@ -13,11 +13,19 @@ new Vue({
         tokenName: '',
         domLoaded: false,
     },
+    watch: {
+        tokenName: function() {
+            if (this.tokenName.replace(/-|\s/g, '').length === 0) {
+                this.tokenName = '';
+            }
+        },
+    },
     mounted: function() {
         window.onload = () => this.domLoaded = true;
     },
     validations: {
         tokenName: {
+            required,
             tokenContain: tokenContain,
             minLength: minLength(4),
             maxLength: maxLength(255),
