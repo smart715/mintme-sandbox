67f80de28c4dcd507852e1863661df54e99d16f7
TheRadly <alex5011999@gmail.com>
2019-08-29 16:11:57 +0300

    [vue] change watch position in other file #issue-5278

--- assets/js/components/modal/TokenEditModal.vue
+++ assets/js/components/modal/TokenEditModal.vue
@@ -82,6 +82,13 @@ export default {
             showTwoFactorModal: false,
         };
     },
+    watch: {
+        newName: function() {
+            if (this.newName.replace(/-|\s/g, '').length === 0) {
+                this.newName = '';
+            }
+        },
+    },
     methods: {
         closeTwoFactorModal: function() {
             this.showTwoFactorModal = false;
