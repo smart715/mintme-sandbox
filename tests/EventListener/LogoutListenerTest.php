046c4daacf45bbb7709ab29b77fe0f0dd8c2ad2b
denlev <levadnyuk66@gmail.com>
2019-09-12 20:51:40 +0300

    [phpunit]Created test for logout listener and corrected previous

--- tests/EventListener/LogoutListenerTest.php
+++ tests/EventListener/LogoutListenerTest.php
@@ -15,13 +15,46 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class LogoutListenerTest extends TestCase
 {
-    public function testOnLogout(): void
+    public function testOnLogoutWithAuthRemembered(): void
     {
         $checker = $this->createMock(AuthorizationCheckerInterface::class);
         $logger = $this->createMock(UserActionLogger::class);
         $session = $this->createMock(SessionInterface::class);
 
-        $checker->method('isGranted')->willReturn(false);
+        $checker->method('isGranted')->with('IS_AUTHENTICATED_REMEMBERED')->willReturn(true);
+
+        $listener = new LogoutListener(
+            $logger,
+            $checker,
+            $session
+        );
+
+        $event = $this->createMock(FilterUserResponseEvent::class);
+
+        $request = $this->createMock(Request::class);
+        $response = $this->createMock(Response::class);
+        $token = $this->createMock(TokenInterface::class);
+        $request->cookies = $this->createMock(ParameterBag::class);
+
+        $event->method('getUser')->willReturn($token);
+        $event->method('getRequest')->willReturn($request);
+
+        $checker->expects($this->once())->method('isGranted')
+            ->with('IS_AUTHENTICATED_REMEMBERED')
+            ->willReturn(false);
+
+        $session->expects($this->once())->method('set');
+
+        $listener->logout($request, $response, $token);
+    }
+
+    public function testOnLogoutWithoutAuthRemembered(): void
+    {
+        $checker = $this->createMock(AuthorizationCheckerInterface::class);
+        $logger = $this->createMock(UserActionLogger::class);
+        $session = $this->createMock(SessionInterface::class);
+
+        $checker->method('isGranted')->with('IS_AUTHENTICATED_REMEMBERED')->willReturn(false);
 
         $listener = new LogoutListener(
             $logger,
@@ -42,6 +75,8 @@ class LogoutListenerTest extends TestCase
         $checker->expects($this->once())->method('isGranted')
             ->with('IS_AUTHENTICATED_REMEMBERED')
             ->willReturn(true);
+
+        $session->expects($this->never())->method('set');
 
         $listener->logout($request, $response, $token);
     }
