afd97895c4b5306d7bc3a89478f6a55eb1374466
Osama Esmail <osamaesmail2@gmail.com>
2019-09-15 14:38:47 +0200

    fix unittests

--- tests/Communications/DeployCostFetcherTest.php
+++ tests/Communications/DeployCostFetcherTest.php
@@ -5,6 +5,8 @@ namespace App\Tests\Communications;
 use App\Communications\DeployCostFetcher;
 use App\Communications\Exception\FetchException;
 use App\Communications\RestRpcInterface;
+use App\Wallet\Money\MoneyWrapperInterface;
+use Money\Currencies;
 use PHPUnit\Framework\TestCase;
 
 class DeployCostFetcherTest extends TestCase
@@ -20,10 +22,10 @@ class DeployCostFetcherTest extends TestCase
         $rpc = $this->createMock(RestRpcInterface::class);
         $rpc->method('send')->willReturn(json_encode($data));
 
-        $fetcher = new DeployCostFetcher($rpc, 49);
+        $fetcher = new DeployCostFetcher($rpc, 49, $this->mockMoneyWrapper());
         $this->assertEquals(
-            '24500.00000000',
-            $fetcher->getDeployWebCost()
+            '24500000000000000000000',
+            $fetcher->getDeployWebCost()->getAmount()
         );
     }
 
@@ -34,10 +36,20 @@ class DeployCostFetcherTest extends TestCase
 
         $this->expectException(FetchException::class);
 
-        $fetcher = new DeployCostFetcher($rpc, 49);
+        $fetcher = new DeployCostFetcher($rpc, 49, $this->mockMoneyWrapper());
         $this->assertEquals(
-            '24500.00000000',
-            $fetcher->getDeployWebCost()
+            '24500000000000000000000',
+            $fetcher->getDeployWebCost()>getAmount()
         );
+    }
+
+    private function mockMoneyWrapper(): MoneyWrapperInterface
+    {
+        $currencies = $this->createMock(Currencies::class);
+        $currencies->method('subunitFor')->willReturn(18);
+        $moneyWrapper = $this->createMock(MoneyWrapperInterface::class);
+        $moneyWrapper->method('getRepository')->willReturn($currencies);
+
+        return $moneyWrapper;
     }
 }
