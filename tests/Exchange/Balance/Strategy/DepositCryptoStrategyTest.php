8b596f695bf79856d44ef71828caa7b10ba3767b
Osama Esmail <osamaesmail2@gmail.com>
2019-09-17 19:28:43 +0200

    unittest strategy

---
+++ tests/Exchange/Balance/Strategy/DepositCryptoStrategyTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Exchange\Balance\Strategy;
+
+use App\Entity\Crypto;
+use App\Entity\User;
+use App\Exchange\Balance\BalanceHandlerInterface;
+use App\Exchange\Balance\Strategy\DepositCryptoStrategy;
+use App\Wallet\Money\MoneyWrapperInterface;
+use Money\Currency;
+use Money\Money;
+use PHPUnit\Framework\MockObject\Matcher\Invocation;
+use PHPUnit\Framework\TestCase;
+
+class DepositCryptoStrategyTest extends TestCase
+{
+    public function testDeposit(): void
+    {
+        $strategy = new DepositCryptoStrategy(
+            $this->mockBalanceHandler($this->once()),
+            $this->mockMoneyWrapper($this->once())
+        );
+
+        $strategy->deposit(
+            $this->createMock(User::class),
+            $this->createMock(Crypto::class),
+            '100'
+        );
+    }
+
+    private function mockBalanceHandler(Invocation $invocation): BalanceHandlerInterface
+    {
+        $handler = $this->createMock(BalanceHandlerInterface::class);
+        $handler->expects($invocation)->method('deposit');
+
+        return $handler;
+    }
+
+    private function mockMoneyWrapper(Invocation $parseInv): MoneyWrapperInterface
+    {
+        $wrapper = $this->createMock(MoneyWrapperInterface::class);
+        $wrapper->expects($parseInv)->method('parse')
+            ->willReturn(new Money('1000000000000000000', new Currency('WEB')));
+
+        return $wrapper;
+    }
+}
