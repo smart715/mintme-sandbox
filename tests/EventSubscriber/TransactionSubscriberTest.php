63761ddd1a48cbd6711c9d84a3a089074767258c
Alejandro Salazar <ale_salazar@outlook.com>
2019-09-22 16:36:29 +0000

    Create TransactionSubscriberTest

---
+++ tests/EventSubscriber/TransactionSubscriberTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\EventSubscriber;
+
+use App\Entity\Crypto;
+use App\Entity\Token\Token;
+use App\Entity\TradebleInterface;
+use App\Entity\User;
+use App\Events\TransactionCompletedEvent;
+use App\EventSubscriber\TransactionSubscriber;
+use App\Mailer\MailerInterface;
+use App\Wallet\Money\MoneyWrapperInterface;
+use Money\Currency;
+use Money\Money;
+use PHPUnit\Framework\TestCase;
+
+class TransactionSubscriberTest extends TestCase
+{
+    public function testOnTransactionCompletedWithCrypto(): void
+    {
+        $subscriber = new TransactionSubscriber(
+            $this->mockMailer(),
+            $this->mockMoneyWrapper()
+        );
+
+        $tradable = $this->createMock(Crypto::class);
+        $tradable->method('getSymbol')->willReturn('WEB');
+
+        $subscriber->onTransactionCompleted(
+            $this->mockTransactionCompletedEvent($tradable, '1')
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function testOnTransactionCompletedWithToken(): void
+    {
+        $subscriber = new TransactionSubscriber(
+            $this->mockMailer(),
+            $this->mockMoneyWrapper()
+        );
+
+        $tradable = $this->createMock(Token::class);
+
+        $subscriber->onTransactionCompleted(
+            $this->mockTransactionCompletedEvent($tradable, '1')
+        );
+
+        $this->assertTrue(true);
+    }
+
+    private function mockMailer(): MailerInterface
+    {
+        return $this->createMock(MailerInterface::class);
+    }
+
+    private function mockMoneyWrapper(): MoneyWrapperInterface
+    {
+        $mw = $this->createMock(MoneyWrapperInterface::class);
+        $mw->method('parse')->willReturnCallback(function (string $amount, string $symbol): Money {
+            return new Money($amount, new Currency($symbol));
+        });
+        $mw->method('format')->willReturnCallback(function (Money $money): string {
+            return $money->getAmount();
+        });
+
+        return $mw;
+    }
+
+    private function mockTransactionCompletedEvent(TradebleInterface $tradable, string $amount): TransactionCompletedEvent
+    {
+        $user = $this->createMock(User::class);
+
+        $event = $this->createMock(TransactionCompletedEvent::class);
+        $event->method('getTradable')->willReturn($tradable);
+        $event->method('getUser')->willReturn($user);
+        $event->method('getAmount')->willReturn($amount);
+
+        return $event;
+    }
+}
