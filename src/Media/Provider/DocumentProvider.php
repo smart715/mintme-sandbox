33caaca5e260cb4bea77f01e7a97057540db81dd
denlev <levadnyuk66@gmail.com>
2019-09-23 22:41:14 +0300

    updated Documents provider

--- src/Media/Provider/DocumentProvider.php
+++ src/Media/Provider/DocumentProvider.php
@@ -2,19 +2,89 @@
 
 namespace App\Media\Provider;
 
-use Sonata\AdminBundle\Form\FormMapper;
+use Gaufrette\Filesystem;
+use Sonata\MediaBundle\CDN\CDNInterface;
+use Sonata\MediaBundle\Generator\GeneratorInterface;
+use Sonata\MediaBundle\Metadata\MetadataBuilderInterface;
+use Sonata\MediaBundle\Model\MediaInterface;
 use Sonata\MediaBundle\Provider\FileProvider;
-use Sonata\MediaBundle\Provider\MetadataInterface;
+use Sonata\MediaBundle\Thumbnail\ThumbnailInterface;
 
 class DocumentProvider extends FileProvider
 {
-    public function getProviderMetadata(): MetadataInterface
+    protected $allowedExtensions;
+
+    protected $allowedMimeTypes;
+
+    protected $metadata;
+
+    /**
+     * @param string                   $name
+     * @param Filesystem               $filesystem
+     * @param CDNInterface             $cdn
+     * @param GeneratorInterface       $pathGenerator
+     * @param ThumbnailInterface       $thumbnail
+     * @param array                    $allowedExtensions
+     * @param array                    $allowedMimeTypes
+     * @param MetadataBuilderInterface $metadata
+     */
+    public function __construct($name, Filesystem $filesystem, CDNInterface $cdn, GeneratorInterface $pathGenerator, ThumbnailInterface $thumbnail, array $allowedExtensions = [], array $allowedMimeTypes = [], MetadataBuilderInterface $metadata = null)
     {
-        return parent::getProviderMetadata();
+        parent::__construct($name, $filesystem, $cdn, $pathGenerator, $thumbnail);
+
+        $this->allowedExtensions = $allowedExtensions;
+        $this->allowedMimeTypes = $allowedMimeTypes;
+        $this->metadata = $metadata;
     }
 
-    public function buildEditForm(FormMapper $formMapper): void
+    /**
+     * {@inheritdoc}
+     */
+    public function postUpdate(MediaInterface $media)
     {
-        parent::buildEditForm($formMapper);
+        if (!$media->getBinaryContent() instanceof \SplFileInfo) {
+            return;
+        }
+
+        // Delete the current file from the FS
+        $oldMedia = clone $media;
+        // if no previous reference is provided, it prevents
+        // Filesystem from trying to remove a directory
+//        dump($this->getFilesystem());
+//        exit;
+        if (null !== $media->getPreviousProviderReference()) {
+            $oldMedia->setProviderReference($media->getPreviousProviderReference());
+
+            $path = $this->getReferenceImage($oldMedia);
+
+            if ($this->getFilesystem()->has($path)) {
+                $this->getFilesystem()->delete($path);
+            }
+        }
+
+        $this->fixBinaryContent($media);
+
+        $this->setFileContents($media);
+
+        $this->generateThumbnails($media);
+
+        $media->resetBinaryContent();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateMetadata(MediaInterface $media, $force = true)
+    {
+        if (!$media->getBinaryContent() instanceof \SplFileInfo) {
+            // this is now optimized at all!!!
+            $path = tempnam(sys_get_temp_dir(), 'sonata_update_metadata_');
+            $fileObject = new \SplFileObject($path, 'w');
+            $fileObject->fwrite($this->getReferenceFile($media)->getContent());
+        } else {
+            $fileObject = $media->getBinaryContent();
+        }
+
+        $media->setSize($fileObject->getSize());
     }
 }
