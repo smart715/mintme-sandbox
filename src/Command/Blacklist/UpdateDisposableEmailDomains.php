cebbc1400561cf667ed9e33c06cc816ca2536f7f
Kruchenov Andrii <kruchenow@gmail.com>
2019-10-01 17:31:40 +0200

    fixed command

--- src/Command/Blacklist/UpdateDisposableEmailDomains.php
+++ src/Command/Blacklist/UpdateDisposableEmailDomains.php
@@ -6,6 +6,7 @@ use App\Communications\DisposableEmailCommunicatorInterface;
 use App\Entity\Blacklist;
 use App\Manager\BlacklistManagerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,6 +14,7 @@ use Symfony\Component\Console\Output\ConsoleSectionOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/* Cron job added to DB. */
 class UpdateDisposableEmailDomains extends Command
 {
     /** @var BlacklistManagerInterface */
@@ -24,11 +26,16 @@ class UpdateDisposableEmailDomains extends Command
     /** @var EntityManagerInterface */
     private $em;
 
+    /** @var LoggerInterface */
+    private $logger;
+
     public function __construct(
+        LoggerInterface $logger,
         BlacklistManagerInterface $blacklistManager,
         DisposableEmailCommunicatorInterface $domainSynchronizer,
         EntityManagerInterface $em
     ) {
+        $this->logger = $logger;
         $this->blacklistManager = $blacklistManager;
         $this->domainSynchronizer = $domainSynchronizer;
         $this->em = $em;
@@ -44,6 +51,8 @@ class UpdateDisposableEmailDomains extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
+        $this->logger->info('[blacklist] Update job started..');
+
         /** @var ConsoleSectionOutput $section */
         $section = $output->section();
         $style = new SymfonyStyle($input, $section);
@@ -69,6 +78,8 @@ class UpdateDisposableEmailDomains extends Command
 
         $section->clear();
         $style->success('Synchronization completed.');
+
+        $this->logger->info('[blacklist] Update job finished..');
     }
 
     /** @param array<Blacklist> $list */
