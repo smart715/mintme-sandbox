0eb68109956e7dc5e92e6436c9cc3f7d2a128eb1
Kruchenov Andrii <kruchenow@gmail.com>
2019-10-01 20:13:16 +0200

    new migration

--- src/Migrations/Version20190924142539.php
+++ src/Migrations/Version20190924142539.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190924142525 extends AbstractMigration
+final class Version20190924142539 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
