dca5bcee8ced418c668d150271cdc007973953bf
Kruchenov Andrii <kruchenow@gmail.com>
2019-10-01 20:06:14 +0200

    new migration

---
+++ src/Migrations/Version20190924142539.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190924142525 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('INSERT INTO cron_job (name, command, schedule, description, enabled) 
+            VALUE ("synchronize-domain", "app:synchronize-domains", "* * * * *", "This command updates black List of domains", 1);');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('DELETE FROM cron_job WHERE name = "synchronize-domains";');
+    }
+}
