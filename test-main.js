1886e6adcfe5163644ea4c3738a884454cdc2c03
Alexey Sobolenko <alexsobolenko@gmail.com>
2019-10-14 12:25:52 +0300

    [test-main.js] set waitSeconds to 0

--- test-main.js
+++ test-main.js
@@ -16,6 +16,8 @@ require.config({
   // Karma serves files under /base, which is the basePath from your config file
   baseUrl: '/base',
 
+  waitSeconds: 0,
+
   // dynamically load all test files
   deps: allTestFiles,
 
