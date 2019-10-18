3c1e0841610a373601d78fc978843e915e987873
Alejandro Salazar <ale_salazar@outlook.com>
2019-10-18 14:21:03 -0400

    fix eslint error

--- assets/js/components/trading/Trading.vue
+++ assets/js/components/trading/Trading.vue
@@ -158,7 +158,7 @@ export default {
                 },
             };
         },
-        fieldsArray: function () {
+        fieldsArray: function() {
             return Object.values(this.fields);
         },
     },
