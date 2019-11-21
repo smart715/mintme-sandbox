244ba0f2f156224c1e78d9acd32259159085c09b
Kruchenov Andrii <kruchenow@gmail.com>
2019-11-21 16:13:27 +0100

    fix Gmail Validation

---
+++ src/Canonicalizer/EmailCanonicalizer.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace App\Canonicalizer;
+
+use FOS\UserBundle\Util\CanonicalizerInterface as FOSUserCanonicalizerInterface;
+
+class EmailCanonicalizer implements FOSUserCanonicalizerInterface
+{
+    /** @var mixed  */
+    private $gmailDomains = ['gmail.com', 'googlemail.com'];
+
+    /** @inheritdoc */
+    public function canonicalize($string): string
+    {
+        if (!$string) {
+            $string = '';
+        }
+
+        $domain = substr($string, strrpos($string, '@') + 1);
+
+        if (in_array($domain, $this->gmailDomains)) {
+            $name = strstr($string, '@', true);
+            $name = str_replace('.', '', strval($name));
+
+            return $name.'@'.$this->gmailDomains[1];
+        }
+
+        return $string;
+    }
+
+}
