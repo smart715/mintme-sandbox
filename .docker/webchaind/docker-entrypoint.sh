755d012b2a5a33e862cdb3fc96711b93ed5d780e
root <root@ubuntu1804.abchosting.abc>
2019-11-22 07:51:19 +0000

    [docker] Switch webchaind to testnet

--- .docker/webchaind/docker-entrypoint.sh
+++ .docker/webchaind/docker-entrypoint.sh
@@ -10,9 +10,10 @@ _RPC_API=${RPC_API:="admin,debug,webchain,miner,eth,net,web3"}
 
 #webchaind  --password ${_PASS} account import ${_ACCOUNT}
 webchaind $(if [ ${_IS_FAST} -eq 1 ]; then echo '--fast'; fi) \
+    --bootnodes enode://68d8691c25e6a6c75f2329de8a50341acad79788318260fa91cc623014d3e5c1eb004148e06f0ac1d24da1b92661dcde244f5e1a1d9714177b638dbbc40a6a6d@95.211.188.144:42447 \
+    --chain=morden \
     --mine \
     --miner-threads 1 \
-    --maxpeers 0 \
     --rpc \
     --rpc-addr "${_RPC_HOST}" \
     --rpc-port "${_RPC_PORT}" \
