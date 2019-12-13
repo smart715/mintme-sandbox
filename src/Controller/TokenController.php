0217e9886c66599b58a2755a00c59517e5a5e501
TheRadly <alex5011999@gmail.com>
2019-12-13 08:12:55 +0300

    [TokenController.php] Addd substr to desc #issue-5686

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -124,7 +124,7 @@ class TokenController extends Controller
 
         return $this->render('pages/pair.html.twig', [
             'token' => $token,
-            'tokenDescription' => $token->getDescription(),
+            'tokenDescription' => substr($token->getDescription(), 0, 200),
             'currency' => Token::WEB_SYMBOL,
             'hash' => $this->getUser() ? $this->getUser()->getHash() : '',
             'profile' => $token->getProfile(),
