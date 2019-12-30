f283356c94d11c29bad0acada7c0e1c672ae4057
denlev <levadnyuk66@gmail.com>
2019-12-30 19:56:26 +0200

    fixed marketCap for BTC WEB

--- assets/js/components/trading/Trading.vue
+++ assets/js/components/trading/Trading.vue
@@ -396,11 +396,20 @@ export default {
             this.sanitizedMarkets = {};
             for (let market in this.markets) {
                 if (this.markets.hasOwnProperty(market)) {
-                    this.markets[market].supply = 1e7;
                     const cryptoSymbol = this.markets[market].base.symbol;
                     const tokenName = this.markets[market].quote.symbol;
                     const marketOnTopIndex = this.getMarketOnTopIndex(cryptoSymbol, tokenName);
                     const tokenized = this.markets[market].quote.deploymentStatus === tokenDeploymentStatus.deployed;
+                    const WebBtcOnTop = this.marketsOnTop[0];
+                    if (marketOnTopIndex > -1 &&
+                        cryptoSymbol === WebBtcOnTop.currency &&
+                        tokenName === WebBtcOnTop.token) {
+                        this.fetchWEBsupply().then((supply) => {
+                            this.markets[market].supply = supply;
+                        });
+                    } else {
+                        this.markets[market].supply = 1e7;
+                    }
 
                     const sanitizedMarket = this.getSanitizedMarket(
                         cryptoSymbol,
@@ -518,7 +527,7 @@ export default {
                 this.$axios.retry.get(this.mintmeSupplyUrl, config)
                     .then((res) => {
                         this.markets['WEBBTC'].supply = res.data;
-                        resolve();
+                        resolve(res.data);
                     })
                     .catch((err) => {
                         this.notifyError('Can not update WEB circulation supply. BTC/WEB market cap might not be accurate.');
@@ -542,7 +551,6 @@ export default {
                 market.base.subunit,
                 false
             );
-
             Vue.set(this.sanitizedMarketsOnTop, 0, market);
         },
         fetchGlobalMarketCap: function() {
