15f6b6475bd318696c8f762e4131ed94c18b4119
yaroslav <yaroslav.artistsoft@gmail.com>
2020-01-04 17:46:02 +0200

    [js] Configured "Deposit more" for BTC and WEB markets. #5705

--- assets/js/mixins/deposit.js
+++ assets/js/mixins/deposit.js
@@ -3,14 +3,30 @@ import {WEB_IDENTIFIER, BTC_IDENTIFIER} from '../utils/constants';
 export default {
     computed: {
         showDepositMoreLink:  function() {
-            return this.loggedIn && [WEB_IDENTIFIER, BTC_IDENTIFIER].includes(this.market.quote.identifier);
+            return this.loggedIn && this.isMarketBTCOrWEB();
         },
     },
     methods: {
         getDepositMoreLink: function () {
-            return this.$routing.generate('wallet', {
-                depositMore: this.market.base.symbol,
-            });
+            if (this.isMarketBTCOrWEB()) {
+                return this.$routing.generate('wallet', {
+                    depositMore: this.getMarketIdentifier(),
+                });
+            }
+        },
+        getMarketIdentifier: function () {
+            if (this.action === 'buy') {
+                return this.market.base.identifier;
+            }
+
+            if (this.action === 'sell') {
+                return this.market.quote.identifier;
+            }
+
+            return '';
+        },
+        isMarketBTCOrWEB: function () {
+            return [WEB_IDENTIFIER, BTC_IDENTIFIER].includes(this.getMarketIdentifier());
         },
     },
 };
