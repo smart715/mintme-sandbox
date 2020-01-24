10a1975de5faf045f5fcb33003e5ec20084ce2e1
yaroslav <yaroslav.artistsoft@gmail.com>
2020-01-24 09:11:01 +0200

    [js] Deposit mixin unit tests. #5705

--- assets/tests/components/TradeSellOrderSpec.js
+++ assets/tests/components/TradeSellOrderSpec.js
@@ -13,8 +13,7 @@ describe('TradeSellOrder', () => {
         moxios.uninstall();
     });
 
-    const $url = 'URL';
-    const $routing = {generate: () => $url};
+    const $routing = {generate: () => 'URL'};
     const localVue = createLocalVue();
     localVue.use(Axios);
     localVue.use(Vuex);
@@ -69,57 +68,6 @@ describe('TradeSellOrder', () => {
         wrapper.vm.sellAmount = 2;
         wrapper.vm.placeOrder();
         done();
-    });
-
-    describe('Deposit more link', function() {
-        it('show deposit more link if logged in', () => {
-            wrapper.vm.loggedIn = false;
-            expect(wrapper.vm.showDepositMoreLink).to.be.false;
-
-            wrapper.vm.loggedIn = true;
-            expect(wrapper.vm.showDepositMoreLink).to.be.true;
-        });
-
-        it('check order input class for not logged/logged in', () => {
-            wrapper.vm.loggedIn = false;
-            expect(wrapper.vm.orderInputClass).to.deep.equal('w-100');
-
-            wrapper.vm.loggedIn = true;
-            expect(wrapper.vm.orderInputClass).to.deep.equal('w-50');
-        });
-
-        it('depositMoreLink', () => {
-            wrapper.vm.action = 'buy';
-            expect(wrapper.vm.depositMoreLink).to.deep.equal($url);
-
-            wrapper.vm.action = 'exchange';
-            expect(wrapper.vm.depositMoreLink).to.deep.equal(undefined);
-
-            wrapper.vm.action = 'sell';
-            expect(wrapper.vm.depositMoreLink).to.deep.equal($url);
-        });
-
-        it('marketIdentifier', () => {
-            wrapper.vm.action = 'buy';
-            expect(wrapper.vm.marketIdentifier).to.deep.equal(wrapper.vm.market.base.identifier);
-
-            wrapper.vm.action = 'exchange';
-            expect(wrapper.vm.marketIdentifier).to.deep.equal('');
-
-            wrapper.vm.action = 'sell';
-            expect(wrapper.vm.marketIdentifier).to.deep.equal(wrapper.vm.market.quote.identifier);
-        });
-
-        it('isMarketBTCOrWEB', () => {
-            wrapper.vm.action = 'buy';
-            expect(wrapper.vm.isMarketBTCOrWEB).to.be.true;
-
-            wrapper.vm.action = 'exchange';
-            expect(wrapper.vm.isMarketBTCOrWEB).to.be.false;
-
-            wrapper.vm.action = 'sell';
-            expect(wrapper.vm.isMarketBTCOrWEB).to.be.true;
-        });
     });
 
     describe('useMarketPrice', () => {
