f692a77476242f3eddb89f346d37e40401cedd89
Alexey Sobolenko <alexsobolenko@gmail.com>
2020-01-28 23:57:04 +0300

    added check for the existence of a token with the selected name

--- assets/js/token_creation.js
+++ assets/js/token_creation.js
@@ -1,6 +1,7 @@
 import Modal from './components/modal/Modal';
 import {required, minLength, maxLength} from 'vuelidate/lib/validators';
 import {
+    HTTP_OK,
     tokenNameValidChars,
     tokenValidFirstChars,
     tokenValidLastChars,
@@ -12,14 +13,40 @@ new Vue({
     components: {
         Modal,
     },
-    data: {
-        tokenName: '',
-        domLoaded: false,
+    data() {
+        return {
+            domLoaded: false,
+            tokenName: '',
+            tokenNameExists: false,
+            tokenNameProcessing: false,
+        };
+    },
+    computed: {
+        saveBtnDisabled: function() {
+            return this.$v.$anyError || !this.tokenName || this.tokenNameExists || this.tokenNameProcessing;
+        },
     },
     watch: {
         tokenName: function() {
             if (this.tokenName.replace(/-|\s/g, '').length === 0) {
                 this.tokenName = '';
+            }
+
+            this.tokenNameExists = false;
+
+            if (!this.$v.tokenName.$invalid && this.tokenName) {
+                this.tokenNameProcessing = true;
+                this.$axios.single.get(this.$routing.generate('check_token_name_exists', {name: this.tokenName}))
+                    .then((response) => {
+                        if (HTTP_OK === response.status) {
+                            this.tokenNameExists = response.data.exists;
+                        }
+                    }, (error) => {
+                        this.$toasted.error('An error has occurred, please try again later');
+                    })
+                    .then(() => {
+                        this.tokenNameProcessing = false;
+                    });
             }
         },
     },
