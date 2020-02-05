7b01135e3b13cc91a3bdd7851f72ae9d9f08bc1a
Dmitry Shmanatov <dmitry.shmanatov@gmail.com>
2020-02-05 15:10:19 +0400

    [js] Remove PairNameMixin from filters #5841

--- assets/js/mixins/filters/index.js
+++ assets/js/mixins/filters/index.js
@@ -1,11 +1,9 @@
 import MoneyFilterMixin from './money';
 import RebrandingFilterMixin from './rebranding';
 import TruncateFilterMixin from './truncate';
-import PairNameMixin from './pair_name';
 
 export {
     MoneyFilterMixin,
     RebrandingFilterMixin,
     TruncateFilterMixin,
-    PairNameMixin,
 };
