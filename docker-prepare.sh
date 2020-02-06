6d187a2e729e5376adfa03bfdd6449f647f8556b
pobo <pobo@MKWK106.dedi.leaseweb.net>
2020-02-06 17:11:09 +0000

    update docker'

--- docker-prepare.sh
+++ docker-prepare.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git submodule update --init --recursive && git submodule update --remote .docker/deposit
-sed -i "s/100000; ++i/100; ++i/" .docker/viabtc/btc/source/marketprice/mp_message.c
+git submodule update --init --recursive && git submodule update --remote .docker/deposit && git submodule update --init --recursive --remote .docker/viabtc/btc/source
+
+#sed -i "s/100000; ++i/100; ++i/" .docker/viabtc/btc/source/marketprice/mp_message.c
