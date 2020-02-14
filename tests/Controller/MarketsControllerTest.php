bafd5894de11b331bb1de0a2f068398183cf4f71
Osama Esmail <osamaesmail2@gmail.com>
2020-02-14 20:55:48 +0200

    markets tests

---
+++ tests/Controller/MarketsControllerTest.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+
+class MarketsControllerTest extends WebTestCase
+{
+    /** @var Client */
+    private $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+    }
+
+    public function testGetMarkets(): void
+    {
+        $this->register($this->client);
+        $this->createProfile($this->client);
+        $tokName = $this->createToken($this->client);
+
+        $this->client->request('GET', '/api/markets');
+
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertCount(2, $res);
+        $this->assertEquals(
+            [
+                $res[0]['base']['symbol'],
+                $res[0]['quote']['symbol'],
+                $res[0]['identifier'],
+                $res[1]['base']['symbol'],
+                $res[1]['quote']['symbol'],
+            ],
+            [
+                'BTC',
+                'WEB',
+                'WEBBTC',
+                'WEB',
+                $tokName,
+            ]
+        );
+    }
+
+    /** @depends testGetMarkets */
+    public function testGetMarketsInfo(): void
+    {
+        $this->register($this->client);
+
+        $this->client->request('GET', '/api/markets/info/1');
+
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertGreaterThan(1, $res['markets']);
+    }
+
+    // todo test getMarketKline()
+
+    public function testGetMarketCap(): void
+    {
+        $this->register($this->client);
+
+        $this->client->request('GET', '/api/markets/marketcap/WEB');
+
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('marketcap', $res);
+    }
+}
