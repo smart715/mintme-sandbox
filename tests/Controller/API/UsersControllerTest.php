c1ce389c04324af65c1acd79f184ea612f1a8c78
Osama Esmail <osamaesmail2@gmail.com>
2020-02-16 20:53:05 +0200

    api/usersController extra tests

--- tests/Controller/API/UsersControllerTest.php
+++ tests/Controller/API/UsersControllerTest.php
@@ -19,24 +19,6 @@ class UsersControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
 
-    public function testCreateApiKeys(): void
-    {
-        $email = $this->register($this->client);
-
-        $this->client->request('POST', '/api/users/keys');
-        $res = json_decode($this->client->getResponse()->getContent(), true);
-
-        /** @var User $user */
-        $user = $this->em->getRepository(User::class)->findOneBy([
-           'email' => $email,
-        ]);
-
-        $this->assertArrayHasKey('publicKey', $res);
-        $this->assertArrayHasKey('plainPrivateKey', $res);
-        $this->assertEquals($res['publicKey'], $user->getApiKey()->getPublicKey());
-        $this->assertEquals('', $user->getApiKey()->getPlainPrivateKey());
-    }
-
     public function testGetApiKeys(): void
     {
         $email = $this->register($this->client);
@@ -55,5 +37,83 @@ class UsersControllerTest extends WebTestCase
 
         $this->assertArrayHasKey('publicKey', $res);
         $this->assertEquals('', $res['plainPrivateKey']);
+    }
+
+    public function testCreateApiKeys(): void
+    {
+        $email = $this->register($this->client);
+
+        $this->client->request('POST', '/api/users/keys');
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy([
+           'email' => $email,
+        ]);
+
+        $this->assertArrayHasKey('publicKey', $res);
+        $this->assertArrayHasKey('plainPrivateKey', $res);
+        $this->assertEquals($res['publicKey'], $user->getApiKey()->getPublicKey());
+        $this->assertEquals('', $user->getApiKey()->getPlainPrivateKey());
+    }
+
+    public function testInvalidateApiKeys(): void
+    {
+        $email = $this->register($this->client);
+
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy([
+            'email' => $email,
+        ]);
+
+        $keys = ApiKey::fromNewUser($user);
+        $this->em->persist($keys);
+        $this->em->flush();
+
+        $this->client->request('DELETE', '/api/users/keys');
+
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy([
+            'email' => $email,
+        ]);
+
+        $this->assertNull($user->getApiKey());
+    }
+
+    public function testCreateApiClient(): void
+    {
+        $email = $this->register($this->client);
+
+        $this->client->request('POST', '/api/users/clients');
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy([
+            'email' => $email,
+        ]);
+
+        $this->assertArrayHasKey('id', $res);
+        $this->assertArrayHasKey('secret', $res);
+        $this->assertEquals($res['id'], $user->getApiClients()[0]['id']);
+        $this->assertArrayNotHasKey('secret', $user->getApiClients()[0]);
+    }
+
+    public function testDeleteApiClient(): void
+    {
+        $email = $this->register($this->client);
+
+        $this->client->request('POST', '/api/users/clients');
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy([
+            'email' => $email,
+        ]);
+        $this->assertCount(1, $user->getApiClients());
+
+        $this->client->request('DELETE', '/api/users/clients/' . $res['id']);
+
+        $this->em->refresh($user);
+        $this->assertCount(0, $user->getApiClients());
     }
 }
