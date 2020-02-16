47b057a6c1edf7e53500394c2257246e21991d44
Osama Esmail <osamaesmail2@gmail.com>
2020-02-16 15:29:07 +0200

    test api/cryptos controller

---
+++ tests/Controller/API/CryptosControllerTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\Controller\API;
+
+use App\Entity\Token\Token;
+use App\Tests\Controller\WebTestCase;
+use App\Utils\DateTime;
+use DateInterval;
+use Symfony\Bundle\FrameworkBundle\Client;
+
+class CryptosControllerTest extends WebTestCase
+{
+    /** @var Client */
+    private $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+    }
+
+    public function testGetRates(): void
+    {
+        $this->register($this->client);
+
+        $this->client->request('GET', '/api/cryptos/rates');
+        $res = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertCount(2, $res);
+        $this->assertArrayHasKey('BTC', $res);
+        $this->assertArrayHasKey('WEB', $res);
+        $this->assertArrayHasKey('BTC', $res['BTC']);
+        $this->assertArrayHasKey('USD', $res['BTC']);
+    }
+}
