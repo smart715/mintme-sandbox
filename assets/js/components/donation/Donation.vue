e713989f48cfbced4726a19c01c0986db52a9a75
yaroslav <yaroslav.artistsoft@gmail.com>
2020-02-19 17:00:57 +0200

    [vue] Donation component front logic. #5804

--- assets/js/components/donation/Donation.vue
+++ assets/js/components/donation/Donation.vue
@@ -9,12 +9,77 @@
                             <span v-else>To make a donation you have to be logged in</span>
                         </div>
                         <div class="donation-body">
-                            <div v-if="loggedIn">
+                            <div v-if="loggedIn" class="h-100">
+                                <div class="p-md-4">
+                                    <div>
+                                        <p>Donation is non-refundable</p>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <p class="mb-2">Currency</p>
+                                            <b-dropdown
+                                                id="donation_currency"
+                                                :text="dropdownText"
+                                                variant="primary"
+                                            >
+                                                <b-dropdown-item v-for="option in options"
+                                                                 :key="option"
+                                                                 :value="option"
+                                                                 @click="selectedCurrency = option">
+                                                    {{ option|rebranding }}
+                                                </b-dropdown-item>
+                                            </b-dropdown>
+                                        </div>
+                                        <div
+                                            v-if="isCurrencySelected"
+                                            class="col"
+                                        >
+                                            <p class="mb-2">Your balance:</p>
+                                            <span class="d-block">{{ balance }}</span>
+                                            <span class="d-block text-danger">Insufficient funds for donation,</span>
+                                            <span class="d-block">please make <a :href="getDepositLink">deposit</a> first</span>
+                                        </div>
+                                    </div>
+                                    <div
+                                        v-if="isCurrencySelected"
+                                        class="w-100"
+                                    >
+                                        <div>
+                                            <label for="amount-to-donate">Amount:</label>
+                                            <div class="input-group">
+                                                <input id="amount-to-donate" type="text" class="form-control">
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-primary" type="button">All</button>
+                                                </div>
+                                            </div>
+                                            <p class="mt-2 mb-4">
+                                                You will receive approximately: {{ getAmountToReceive }} tokens
+                                                <guide
+                                                    :placement="'right-start'"
+                                                    :max-width="'200px'"
+                                                >
+                                                    <template slot="body">
+                                                        Amount of tokens you will receive may vary greatly depending on the current situation.
+                                                        It can be much less than suggested here.
+                                                    </template>
+                                                </guide>
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p class="mb-2">Fee for donation: {{ getDonationFee }}%</p>
+                                            <button class="btn btn-primary">
+                                                Donate {{ donationCurrency }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div v-if="!contentLoaded" class="p-5 text-center">
                                 <font-awesome-icon icon="circle-notch" spin class="loading-spinner" fixed-width />
                             </div>
-                            <div id="tab-login-form-container" class="p-md-4"></div>
+                            <div
+                                id="tab-login-form-container"
+                                :class="loginFormContainerClass"></div>
                         </div>
                     </div>
                 </div>
@@ -24,13 +89,15 @@
 </template>
 
 <script>
-import {NotificationMixin, LoggerMixin} from '../../mixins';
+import {NotificationMixin, LoggerMixin, RebrandingFilterMixin} from '../../mixins';
+import Guide from '../Guide';
+import {webSymbol, btcSymbol} from '../../utils/constants';
 
 export default {
     name: 'Donation',
-    mixins: [NotificationMixin, LoggerMixin],
+    mixins: [NotificationMixin, LoggerMixin, RebrandingFilterMixin],
     components: {
-
+        Guide,
     },
     props: {
         market: Object,
@@ -41,15 +108,53 @@ export default {
     },
     data() {
         return {
+            options: {
+                webSymbol,
+                btcSymbol,
+            },
+            selectedCurrency: null,
             contentLoaded: false,
+            donationFee: 0,
+            amountToReceive: 0,
+            balance: 0,
         };
     },
     computed: {
+        loginFormContainerClass: function() {
+            if (!this.loggedIn) {
+                return 'p-md-4';
+            }
 
+            return '';
+        },
+        donationCurrency: function() {
+            return this.rebrandingFunc(this.selectedCurrency);
+        },
+        getDonationFee: function() {
+            return this.donationFee;
+        },
+        getAmountToReceive: function() {
+            return this.amountToReceive;
+        },
+        getDepositLink: function() {
+            return this.$routing.generate('wallet', {
+                depositMore: this.selectedCurrency,
+            });
+        },
+        isCurrencySelected: function() {
+            return [webSymbol, btcSymbol].includes(this.selectedCurrency);
+        },
+        dropdownText: function() {
+            return this.isCurrencySelected
+                ? this.donationCurrency
+                : 'Select currency';
+        },
     },
     mounted() {
         if (!this.loggedIn) {
             this.loadLoginForm();
+        } else {
+            this.contentLoaded = true;
         }
     },
     methods: {
