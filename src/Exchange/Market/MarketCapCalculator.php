8441b9632d097f05aae4637ce203b173dbc47a4c
mryndin <softarts@mail.ru>
2020-02-20 13:29:33 +0000

    [php] fix User usage in UserController.php #5873

--- src/Exchange/Market/MarketCapCalculator.php
+++ src/Exchange/Market/MarketCapCalculator.php
@@ -55,7 +55,7 @@ class MarketCapCalculator
         $this->tokenSupply = $tokenSupply;
         $this->moneyWrapper = $moneyWrapper;
         $this->rpc = $rpc;
-        
+
         /** @var MarketStatusRepository $newRepository */
         $newRepository = $em->getRepository(MarketStatus::class);
 
