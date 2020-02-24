797fad66ab15fb6c2295fb7ab9fe498c4ab37c6e
Osama Esmail <osamaesmail2@gmail.com>
2020-02-24 09:07:47 +0200

    test registration

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -2,9 +2,6 @@
 
 namespace App\Tests\Controller;
 
-use App\Entity\GoogleAuthenticatorEntry;
-use App\Entity\User;
-use App\Manager\TwoFactorManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Client;
 
 class SecurityControllerTest extends WebTestCase
