c0d9880e63b92ecbe87f5d8ec2a730eda33f3e71
mryndin <softarts@mail.ru>
2020-03-01 05:40:51 +0000

    [vue] Move spinner component to mixins #5745

--- assets/js/mixins/nested_spinner.js
+++ assets/js/mixins/nested_spinner.js
@@ -1,20 +1,39 @@
 export default {
+  props: {
+    spinnerDiv: {
+      type: String,
+      required: false,
+      defaultValue: 'page-load-spinner',
+    },
+  },
   data() {
     return {
       spinnerQuantity: 0,
     };
   },
   methods: {
+    directShowSpinner: function() {
+      this.spinnerQuantity = 0;
+      document.getElementById(this.spinnerDiv).classList.add('hidden');
+    },
+    directHideSpinner: function() {
+      this.spinnerQuantity = 0;
+      if (document.getElementById(this.spinnerDiv).classList.contains('hidden')) {
+        document.getElementById(this.spinnerDiv).classList.remove('hidden');
+      }
+    },
     showSpinner: function() {
       if (!this.spinnerQuantity) {
-        this.$refs.spinner.show();
+        this.directShowSpinner();
       }
       this.spinnerQuantity = this.spinnerQuantity + 1;
     },
     hideSpinner: function() {
-      this.spinnerQuantity = this.spinnerQuantity - 1;
-      if (!this.spinnerQuantity) {
-        this.$refs.spinner.hide();
+      if (this.spinnerQuantity) {
+        this.spinnerQuantity = this.spinnerQuantity - 1;
+        if (!this.spinnerQuantity) {
+          this.directHideSpinner();
+        }
       }
     },
   },
