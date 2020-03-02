949ce3708285ef1a8b00db402b107bea01d03c32
Alejandro Salazar <ale_salazar@outlook.com>
2020-03-02 14:26:15 -0400

    Create new column minted_amount on token table that is updated by gateway via consumer

--- src/Consumers/ContractUpdateConsumer.php
+++ src/Consumers/ContractUpdateConsumer.php
@@ -4,7 +4,10 @@ namespace App\Consumers;
 
 use App\Consumers\Helpers\DBConnection;
 use App\Entity\Token\Token;
+use App\SmartContract\Model\ChangeMintDestinationCallbackMessage;
 use App\SmartContract\Model\ContractUpdateCallbackMessage;
+use App\SmartContract\Model\UpdateMintedAmountCallbackMessage;
+use App\Wallet\Money\MoneyWrapperInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
@@ -18,12 +21,17 @@ class ContractUpdateConsumer implements ConsumerInterface
     /** @var EntityManagerInterface */
     private $em;
 
+    /** @var MoneyWrapperInterface */
+    private $moneyWrapper;
+
     public function __construct(
         LoggerInterface $logger,
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        MoneyWrapperInterface $moneyWrapper
     ) {
         $this->logger = $logger;
         $this->em = $em;
+        $this->moneyWrapper = $moneyWrapper;
     }
 
     /** {@inheritdoc} */
@@ -44,6 +52,35 @@ class ContractUpdateConsumer implements ConsumerInterface
             return true;
         }
 
+        if ('updateMintDestination' === $clbResult->getMethod()) {
+            try {
+                $clbMessage = ChangeMintDestinationCallbackMessage::parse(json_decode($clbResult->getMessage(), true));
+            } catch (\Throwable $exception) {
+                $this->logger->warning("[contract-update-consumer] Failed to parse incoming message", [$clbResult->getMessage()]);
+
+                return true;
+            }
+
+            return $this->updateMintDestination($clbMessage);
+        } elseif ('updateMintedAmount' === $clbResult->getMethod()) {
+            try {
+                $clbMessage = UpdateMintedAmountCallbackMessage::parse(json_decode($clbResult->getMessage(), true));
+            } catch (\Throwable $exception) {
+                $this->logger->warning("[contract-update-consumer] Failed to parse incoming message", [$clbResult->getMessage()]);
+
+                return true;
+            }
+
+            return $this->updateMintedAmount($clbMessage);
+        } else {
+            $this->logger->warning("[contract-update-consumer] Invalid method", [$clbResult->getMethod()]);
+
+            return true;
+        }
+    }
+
+    private function updateMintDestination(ChangeMintDestinationCallbackMessage $clbResult): bool
+    {
         try {
             $repo = $this->em->getRepository(Token::class);
             $token = $repo->findOneBy(['address' => $clbResult->getTokenAddress()]);
@@ -60,6 +97,33 @@ class ContractUpdateConsumer implements ConsumerInterface
             $this->em->flush();
         } catch (\Throwable $exception) {
             $this->logger->error("[contract-update-consumer] Failed to update token address. Retry operation. Reason: {$exception->getMessage()}");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private function updateMintedAmount(UpdateMintedAmountCallbackMessage $clbResult): bool
+    {
+        try {
+            $repo = $this->em->getRepository(Token::class);
+            $token = $repo->findOneBy(['name' => $clbResult->getTokenName()]);
+
+            if (!$token) {
+                $this->logger->info("[contract-update-consumer] Invalid token name '{$clbResult->getTokenName()}' given");
+
+                return true;
+            }
+
+            $minted = $this->moneyWrapper->parse($clbResult->getValue(), Token::TOK_SYMBOL);
+
+            $token->setMintedAmount($token->getMintedAmount()->add($minted));
+
+            $this->em->persist($token);
+            $this->em->flush();
+        } catch (\Throwable $exception) {
+            $this->logger->error("[contract-update-consumer] Failed to update token minted amount. Retry operation. Reason: {$exception->getMessage()}");
 
             return false;
         }
