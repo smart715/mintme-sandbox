e4b412fc89bec1fce29a455c5a7b53f82ac72b6a
sambasten <akinwumisamuel@gmail.com>
2020-03-13 22:34:33 +0000

    [vue] fixed indentation issue-5696

--- assets/js/components/trade/TradeSellOrder.vue
+++ assets/js/components/trade/TradeSellOrder.vue
@@ -44,17 +44,13 @@
                             <div v-if="loggedIn && immutableBalance" class="w-50 m-auto pl-4">
                                 Your
                                 <span>
-                                    <span 
-                                        v-if="market.quote.symbol.length > 17" 
-                                        class="c-pointer" 
+                                    <span v-if="market.quote.symbol.length > 17" class="c-pointer" 
                                         @click="balanceClicked"
                                         v-b-tooltip="{title: rebrandingFunc(market.quote.symbol), boundary:'viewport'}"
                                     >
                                         {{ market.quote.symbol | rebranding | truncate(17) }} :
                                     </span>
-                                    <span 
-                                        v-else 
-                                        class="c-pointer" 
+                                    <span v-else class="c-pointer" 
                                         @click="balanceClicked"
                                     >
                                         {{ market.quote.symbol | rebranding }} :
