34c785f07b78140a671ae05f9fd4e695560896ce
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-03-17 19:36:29 +0200

    [php] Airdrop participant entity. #5969

---
+++ src/Repository/AirdropCampaign/ParticipantRepository.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace App\Repository\AirdropCampaign;
+
+use App\Entity\AirdropCampaign\Participant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Participant|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Participant|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Participant[]    findAll()
+ * @method Participant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ParticipantRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Participant::class);
+    }
+}
