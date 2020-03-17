cef077eb01bbf57a8c48967b49d76a6982e87f52
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-03-17 19:53:53 +0200

    [php] Migrationns. New tables `airdrop` and `airdrop_participants`. #5969

---
+++ src/Migrations/Version20200316165922.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200316165922 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE airdrop (
+                                id INT AUTO_INCREMENT NOT NULL,
+                                token_id INT NOT NULL,
+                                status SMALLINT NOT NULL,
+                                amount VARCHAR(100) NOT NULL,
+                                participants INT NOT NULL,
+                                actual_amount VARCHAR(100) DEFAULT NULL,
+                                actual_participants INT DEFAULT NULL,
+                                INDEX IDX_4D15AF2C41DEE7B9 (token_id),
+                                PRIMARY KEY(id)
+                            )
+                            DEFAULT CHARACTER SET utf8mb4
+                            COLLATE utf8mb4_unicode_ci ENGINE = InnoDB'
+        );
+        $this->addSql('ALTER TABLE airdrop ADD CONSTRAINT FK_4D15AF2C41DEE7B9 FOREIGN KEY (token_id) REFERENCES token (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE airdrop');
+    }
+}
