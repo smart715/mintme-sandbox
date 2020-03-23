798d6bba2092e6fd2e0193b9c9d76a4bae0ded61
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-03-23 14:00:41 +0200

    [php] Unit tests. #5969

---
+++ tests/Manager/AirdropCampaignManagerTest.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\Manager;
+
+use App\Entity\AirdropCampaign\Airdrop;
+use App\Entity\Token\Token;
+use App\Entity\User;
+use App\Manager\AirdropCampaignManager;
+use App\Repository\AirdropCampaign\AirdropParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class AirdropCampaignManagerTest extends TestCase
+{
+    public function testCreateAirdrop(): void
+    {
+        /** @var EntityManagerInterface|MockObject $em */
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->exactly(2))->method('persist');
+        $em->expects($this->exactly(2))->method('flush');
+        /** @var Token|MockObject $em */
+        $token = $this->createMock(Token::class);
+
+        $airdropManager = new AirdropCampaignManager($em);
+
+        $airdropManager->createAirdrop(
+            $token,
+            '500',
+            150,
+            new \DateTimeImmutable('+2 days')
+        );
+    }
+
+    public function testDeleteAirdrop(): void
+    {
+        /** @var EntityManagerInterface|MockObject $em */
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())->method('persist');
+        $em->expects($this->once())->method('flush');
+        $airdrop = new Airdrop();
+        $airdrop->setStatus(Airdrop::STATUS_ACTIVE);
+
+        $airdropManager = new AirdropCampaignManager($em);
+        $airdropManager->deleteAirdrop($airdrop);
+
+        $this->assertEquals(Airdrop::STATUS_REMOVED, $airdrop->getStatus());
+    }
+
+    public function testDeleteActiveAirdrop(): void
+    {
+        /** @var EntityManagerInterface|MockObject $em */
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())->method('persist');
+        $em->expects($this->once())->method('flush');
+        $airdrop = new Airdrop();
+        $airdrop->setStatus(Airdrop::STATUS_ACTIVE);
+        /** @var Token|MockObject $em */
+        $token = $this->createMock(Token::class);
+        $token->expects($this->once())->method('getActiveAirdrop')->willReturn($airdrop);
+
+        $airdropManager = new AirdropCampaignManager($em);
+        $airdropManager->deleteActiveAirdrop($token);
+
+        $this->assertEquals(Airdrop::STATUS_REMOVED, $airdrop->getStatus());
+    }
+
+    public function testShowAirdropCampaign(): void
+    {
+        /** @var AirdropParticipantRepository|MockObject $repository */
+        $repository = $this->createMock(AirdropParticipantRepository::class);
+        $repository
+            ->expects($this->once())
+            ->method('getParticipantByUserAndToken')
+            ->willReturn(null);
+
+        /** @var EntityManagerInterface|MockObject $em */
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em
+            ->expects($this->once())
+            ->method('getRepository')
+            ->willReturn($repository);
+
+        /** @var Token|MockObject $em */
+        $token = $this->createMock(Token::class);
+        /** @var User|MockObject $user */
+        $user = $this->createMock(User::class);
+
+        $airdropManager = new AirdropCampaignManager($em);
+
+        $this->assertFalse($airdropManager->showAirdropCampaign(null, $token));
+        $this->assertFalse($airdropManager->showAirdropCampaign($user, $token));
+
+        $airdrop = new Airdrop();
+        $airdrop->setStatus(Airdrop::STATUS_ACTIVE);
+        $token->expects($this->exactly(2))->method('getActiveAirdrop')->willReturn($airdrop);
+
+        $this->assertTrue($airdropManager->showAirdropCampaign($user, $token));
+    }
+}
