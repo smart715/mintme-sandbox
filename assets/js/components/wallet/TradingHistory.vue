39f426001568ef42806b14c2e949c1fcc63300c1
Akinwumi Samuel <akinwumisamuel@gmail.com>
2020-04-07 10:06:56 +0000

    Update TradingHistory.vue sorting

--- assets/js/components/wallet/TradingHistory.vue
+++ assets/js/components/wallet/TradingHistory.vue
@@ -9,7 +9,7 @@
                     :sort-compare="sortCompare"
                     :sort-by="fields.date.key"
                     :sort-desc="true"
-                    sort-direction
+                    sort-direction="asc"
                     sort-icon-left
                 >
                     <template v-slot:cell(name)="row">
