b3c4f21fb3407a2b4c9b4a2d476cf86a7cdff3c1
dan <danilznz@gmail.com>
2020-04-08 10:59:44 +0200

    [php] bcmul fixed #6043

--- src/Utils/Validator/MinOrderValidator.php
+++ src/Utils/Validator/MinOrderValidator.php
@@ -61,7 +61,7 @@ class MinOrderValidator implements ValidatorInterface
 
         return $this->price >= $baseMinimal
             && $this->amount >= $quoteMinimal
-            && BigDecimal::of($this->price)->multipliedBy((string)$this->amount) >= $baseMinimal;
+            && (string)BigDecimal::of((float)$this->price)->multipliedBy((float)$this->amount) >= $baseMinimal;
     }
 
     private function getCrypto(?TradebleInterface $tradeble): ?Crypto
