3ea9443b2a91308c149b651ec136840dbf39a55a
Alejandro Salazar <ale_salazar@outlook.com>
2020-04-09 21:35:26 +0000

    Fix linting errors

--- assets/js/components/token/posts/Post.vue
+++ assets/js/components/token/posts/Post.vue
@@ -1,19 +1,19 @@
 <template>
-	<div class="post">
-		<p>{{ post.content }}</p>
-		<span>{{ post.createdAt }}</span>
-		<span>{{ post.author }}</span>
-	</div>
+    <div class="post">
+        <p>{{ post.content }}</p>
+        <span>{{ post.createdAt }}</span>
+        <span>{{ post.author }}</span>
+    </div>
 </template>
 
 <script>
 export default {
-	name: "Post",
-	props: {
-		post: Object,
-	},
-	data() {
-		return {};
-	},
-}
+    name: 'Post',
+    props: {
+        post: Object,
+    },
+    data() {
+        return {};
+    },
+};
 </script>
--- assets/js/components/token/posts/Posts.vue
+++ assets/js/components/token/posts/Posts.vue
@@ -1,45 +1,45 @@
 <template>
-	<div class="card h-100">
-		<div class="card-header">
-			<slot name="title">Posts</slot>
-		</div>
-		<div class="card-body posts">
-			<post v-for="(post, key) in posts" :post="post" :key="key"></post>
-		</div>
-	</div>
+    <div class="card h-100">
+        <div class="card-header">
+            <slot name="title">Posts</slot>
+        </div>
+        <div class="card-body posts">
+            <post v-for="(post, key) in posts" :post="post" :key="key"></post>
+        </div>
+    </div>
 </template>
 
 <script>
 import Post from './Post';
 
 export default {
-	name: 'Posts',
-	components: {Post},
-	data() {
-		return {
-			posts: [
-				{
-					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
-					createdAt: '09.04.2020',
-					author: 'alejandrojsn',
-				},
-				{
-					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
-					createdAt: '09.04.2020',
-					author: 'alejandrojsn',
-				},
-				{
-					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
-					createdAt: '09.04.2020',
-					author: 'alejandrojsn',
-				},
-				{
-					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
-					createdAt: '09.04.2020',
-					author: 'alejandrojsn',
-				}
-			],
-		};
-	},
-}
+    name: 'Posts',
+    components: {Post},
+    data() {
+        return {
+            posts: [
+                {
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+                    createdAt: '09.04.2020',
+                    author: 'alejandrojsn',
+                },
+                {
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+                    createdAt: '09.04.2020',
+                    author: 'alejandrojsn',
+                },
+                {
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+                    createdAt: '09.04.2020',
+                    author: 'alejandrojsn',
+                },
+                {
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+                    createdAt: '09.04.2020',
+                    author: 'alejandrojsn',
+                },
+            ],
+        };
+    },
+};
 </script>
