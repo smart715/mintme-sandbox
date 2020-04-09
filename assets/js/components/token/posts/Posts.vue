3098bc2d6f8d9540292b204ef3ab8fd73360bdaa
Alejandro Salazar <ale_salazar@outlook.com>
2020-04-09 19:47:01 +0000

    Created posts component

---
+++ assets/js/components/token/posts/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="card h-100">
+		<div class="card-header">
+			<slot name="title">Posts</slot>
+		</div>
+		<div class="card-body">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reiciendis, aspernatur quos iusto hic exercitationem corporis necessitatibus nulla aliquam deleniti laudantium maiores, labore pariatur porro sequi excepturi, ipsa distinctio quidem?
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Posts',
+	data() {
+		return {
+			hola: 'hola',
+		}
+	},
+}
+</script>
