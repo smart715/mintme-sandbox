71c1fda8e07c0e4fa1209468e1753c24efc581f9
Alejandro Salazar <ale_salazar@outlook.com>
2020-04-09 21:11:58 +0000

    Create Post component

---
+++ assets/js/components/token/posts/Post.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="post">
+		<p>{{ post.content }}</p>
+		<span>{{ post.createdAt }}</span>
+		<span>{{ post.author }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Post",
+	props: {
+		post: Object,
+	},
+	data() {
+		return {};
+	},
+}
+</script>
--- assets/js/components/token/posts/Posts.vue
+++ assets/js/components/token/posts/Posts.vue
@@ -3,19 +3,43 @@
 		<div class="card-header">
 			<slot name="title">Posts</slot>
 		</div>
-		<div class="card-body">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reiciendis, aspernatur quos iusto hic exercitationem corporis necessitatibus nulla aliquam deleniti laudantium maiores, labore pariatur porro sequi excepturi, ipsa distinctio quidem?
+		<div class="card-body posts">
+			<post v-for="(post, key) in posts" :post="post" :key="key"></post>
 		</div>
 	</div>
 </template>
 
 <script>
+import Post from './Post';
+
 export default {
 	name: 'Posts',
+	components: {Post},
 	data() {
 		return {
-			hola: 'hola',
-		}
+			posts: [
+				{
+					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+					createdAt: '09.04.2020',
+					author: 'alejandrojsn',
+				},
+				{
+					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+					createdAt: '09.04.2020',
+					author: 'alejandrojsn',
+				},
+				{
+					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+					createdAt: '09.04.2020',
+					author: 'alejandrojsn',
+				},
+				{
+					content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quam eum, odio rem beatae aliquam voluptatem iste consequuntur esse maxime, aspernatur delectus similique amet! Laudantium, quis impedit ipsum tenetur ipsam!',
+					createdAt: '09.04.2020',
+					author: 'alejandrojsn',
+				}
+			],
+		};
 	},
 }
 </script>
