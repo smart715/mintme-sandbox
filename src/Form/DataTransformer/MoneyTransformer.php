7f6ca259f1891e6728bfa726ac7062477c64d8bb
Alejandro Salazar <ale_salazar@outlook.com>
2020-04-16 23:52:04 +0000

    Implement MoneyTransformer

--- src/Form/DataTransformer/MoneyTransformer.php
+++ src/Form/DataTransformer/MoneyTransformer.php
@@ -2,21 +2,49 @@
 
 namespace App\Form\DataTransformer;
 
+use App\Entity\Token\Token;
+use App\Wallet\Money\MoneyWrapperInterface;
 use Money\Currency;
 use Money\Money;
 use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class MoneyTransformer implements DataTransformerInterface
 {
+    /** @var MoneyWrapperInterface */
+    private $moneyWrapper;
+
+    /** @var string */
+    private $symbol;
+
+    public function __construct(MoneyWrapperInterface $moneyWrapper)
+    {
+        $this->moneyWrapper = $moneyWrapper;
+    }
+
     /** @inheritdoc */
     public function transform($value): string
     {
-        return $value->getAmount();
+        return $this->moneyWrapper->format($value);
     }
     
     /** @inheritdoc */
     public function reverseTransform($value): Money
     {
-        return new Money($value, new Currency('TOK'));
+        try {
+            return $this->moneyWrapper->parse($value, $this->symbol ?? Token::TOK_SYMBOL);
+        } catch(\Throwable $e) {
+            throw new TransformationFailedException();
+        }
+    }
+
+    /**
+     * Use this function on your FormType to set the symbol of the generated Money object
+     */
+    public function setSymbol(string $symbol): self
+    {
+        $this->symbol = $symbol;
+
+        return $this;
     }
 }
