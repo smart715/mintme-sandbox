6bed7cc1b5ae52b66e772e54c31d63303b9962a2
Alejandro Salazar <ale_salazar@outlook.com>
2020-04-18 22:46:55 +0000

    Extract checkInput to a mixin to be used in different components

--- assets/js/components/token/posts/CreatePost.vue
+++ assets/js/components/token/posts/CreatePost.vue
@@ -13,8 +13,8 @@
                     name="amount"
                     type="text"
                     v-model="amount"
-                    @keypress="checkInput"
-                    @paste="checkInput"
+                    @keypress="checkInput(4, 6)"
+                    @paste="checkInput(4, 6)"
                 >
                 <div class="invalid-feedback">
                     {{ amountErrorMessage }}
@@ -46,10 +46,14 @@
 <script>
 import BbcodeEditor from '../../bbcode/BbcodeEditor';
 import BbcodeHelp from '../../bbcode/BbcodeHelp';
+import {CheckInputMixin} from '../../../mixins';
 import {required, minLength, maxLength, decimal, between} from 'vuelidate/lib/validators';
 
 export default {
 	name: 'CreatePost',
+    mixins: [
+        CheckInputMixin,
+    ],
 	components: {BbcodeEditor, BbcodeHelp},
 	data() {
 		return {
@@ -113,22 +117,6 @@ export default {
                 content: this.content,
                 amount: this.amount,
             }).then(console.log, console.log);
-        },
-        checkInput: function() {
-            let selectionStart = event.target.selectionStart;
-            let selectionEnd = event.target.selectionEnd;
-            let amount = event.srcElement.value;
-            let regex = new RegExp(`^[0-9]{0,6}(\\.[0-9]{0,4})?$`);
-            let input = event instanceof ClipboardEvent
-                ? event.clipboardData.getData('text')
-                : String.fromCharCode(!event.charCode ? event.which : event.charCode);
-
-            if (!regex.test(amount.slice(0, selectionStart) + input + amount.slice(selectionEnd))) {
-                event.preventDefault();
-                return false;
-            }
-
-            return true;
         },
     },
     validations() {
--- assets/js/mixins/index.js
+++ assets/js/mixins/index.js
@@ -1,27 +1,29 @@
+import CheckInputMixin from './check_input';
 import LazyScrollTableMixin from './lazy_scroll_table';
-import WebSocketMixin from './websocket';
+import LoggerMixin from './logger';
+import NotificationMixin from './notification';
+import OrderClickedMixin from './order_clicked';
+import OrderMixin from './order';
+import PairNameMixin from './pair_name';
 import PricePositionMixin from './price_position';
 import PlaceOrder from './place_order';
-import OrderClickedMixin from './order_clicked';
-import {MoneyFilterMixin, RebrandingFilterMixin, TruncateFilterMixin as FiltersMixin} from './filters';
-import NotificationMixin from './notification';
 import TraderHoveredMixin from './trader_hovered';
-import OrderMixin from './order';
-import LoggerMixin from './logger';
-import PairNameMixin from './pair_name';
+import WebSocketMixin from './websocket';
+import {MoneyFilterMixin, RebrandingFilterMixin, TruncateFilterMixin as FiltersMixin} from './filters';
 
 export {
+    CheckInputMixin,
     FiltersMixin,
     LazyScrollTableMixin,
+    LoggerMixin,
     MoneyFilterMixin,
     NotificationMixin,
     OrderClickedMixin,
+    OrderMixin,
+    PairNameMixin,
     PlaceOrder,
     PricePositionMixin,
     RebrandingFilterMixin,
-    WebSocketMixin,
     TraderHoveredMixin,
-    OrderMixin,
-    LoggerMixin,
-    PairNameMixin,
+    WebSocketMixin,
 };
