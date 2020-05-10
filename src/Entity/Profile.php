2c97134b64f89ca7a004cf538cdbefb15eaa221e
victoracho <victoracho.box@gmail.com>
2020-05-10 05:20:56 +0000

    [php, js, twig] added chinese names validation issue-6070

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -278,8 +278,8 @@ class Profile
     }
 
    /**
-    * @Assert\Callback
-    */
+   * @Assert\Callback
+   */
     public function validateFirstName(ExecutionContextInterface $context, string $payload): ?string
     {
         if (false == preg_match("/^\p{Han}{2,10}+$/u", $this->getFirstName()) ) {
@@ -292,18 +292,18 @@ class Profile
         }
     }
 
-    /**
-     * @Assert\Callback
-     */
-     public function validateLastName(ExecutionContextInterface $context, string $payload): ?string
-     {
-         if (false == preg_match("/^\p{Han}{2,10}+$/u", $this->getLastName()) ) {
-             // if the first name has  any chinese characters nothing happens
-             if ( 2 > strlen($this->getLastName())) {
-                 $context->buildViolation('This value is too short. It should have 2 characters or more.')
-                 ->atPath('firstName')
-                 ->addViolation();
-             }
-         }
-     }
+  /**
+  * @Assert\Callback
+  */
+   public function validateLastName(ExecutionContextInterface $context, string $payload): ?string
+   {
+       if (false == preg_match("/^\p{Han}{2,10}+$/u", $this->getLastName()) ) {
+           // if the first name has  any chinese characters nothing happens
+           if ( 2 > strlen($this->getLastName())) {
+               $context->buildViolation('This value is too short. It should have 2 characters or more.')
+               ->atPath('firstName')
+               ->addViolation();
+           }
+       }
+   }
 }
