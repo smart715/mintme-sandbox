52afeba660bf1b53352bc131753272f911b046e8
victoracho <victoracho.box@gmail.com>
2020-05-10 08:16:43 +0000

    [php, js, twig] added chinese names validation issue-6070

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -280,7 +280,7 @@ class Profile
    /**
    * @Assert\Callback
    */
-    public function validateFirstName(ExecutionContextInterface $context, $payload = null): void
+    public function validateFirstName(ExecutionContextInterface $context, ?string $payload): void
     {
         if (false == preg_match("/^\p{Han}{2,10}+$/u", strval($this->getFirstName()))) {
             // if the first name has  any chinese characters nothing happens
@@ -295,7 +295,7 @@ class Profile
   /**
   * @Assert\Callback
   */
-    public function validateLastName(ExecutionContextInterface $context, $payload = null): void
+    public function validateLastName(ExecutionContextInterface $context, ?string $payload): void
     {
         if (false == preg_match("/^\p{Han}{2,10}+$/u", strval($this->getLastName()))) {
             // if the first name has  any chinese characters nothing happens
