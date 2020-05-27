99904d09bcafaca9ee8434488354e7ea812eadb8
Alejandro Salazar <ale_salazar@outlook.com>
2020-05-27 01:49:20 +0000

    phpcbf

--- src/Controller/TradingController.php
+++ src/Controller/TradingController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Token\Token;
 use App\Repository\TokenRepository;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
