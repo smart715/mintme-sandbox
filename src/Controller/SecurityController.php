6bd465f5ba5b82e39a3434e80530b54f139b1c56
dan <danilznz@gmail.com>
2020-05-29 15:06:57 +0200

    [php] import order altered #6211

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,10 +11,10 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SecurityController extends FOSSecurityController
 {
