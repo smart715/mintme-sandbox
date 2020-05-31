caed3dff08a5b883eb5100572d74b9d82c767cf7
victoracho <victoracho.box@gmail.com>
2020-05-30 22:32:17 -0400

    [php,js] added telegram and discord backend validation issue-6165

--- assets/js/components/token/TokenDiscordChannel.vue
+++ assets/js/components/token/TokenDiscordChannel.vue
@@ -151,10 +151,10 @@ export default {
                        this.editing = false;
                     }
                     this.submitting = false;
-                }).(catch(error) => {
+                }).catch(error) {
                     this.notifyError(error.response.data.message);
                     this.sendLogs('error', 'Can not save discord', response);
-            });
+            };
         },
         toggleEdit: function() {
             this.editing = !this.editing;
