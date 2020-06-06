352971950328386ce90e5356b795210d63b612d4
sambasten <akinwumisamuel@gmail.com>
2020-06-06 00:42:55 +0000

    [vue] cleand tokendeployicon comp

--- assets/js/components/token/deploy/TokenDeployIcon.vue
+++ assets/js/components/token/deploy/TokenDeployIcon.vue
@@ -39,32 +39,6 @@ export default {
         isOwner: Boolean,
         statusProp: String,
     },
-    // watch: {
-    //     statusProp: function() {
-    //         this.showPending = true;
-    //         this.deployed = false;
-
-    //         clearInterval(this.deployInterval);
-    //         this.deployInterval = setInterval(() => {
-    //             this.$axios.single.get(this.$routing.generate('is_token_deployed', {name: this.name}))
-    //             .then((response) => {
-    //                     if (true === response.data.deployed) {
-    //                         clearInterval(this.deployInterval);
-    //                         this.status = tokenDeploymentStatus.deployed;
-    //                         this.deployed = true;
-    //                         this.showPending = false;
-    //                         this.$emit('deployed');
-    //                     }
-    //                     this.retryCount++;
-    //                     if (this.retryCount >= this.retryCountLimit) {
-    //                         clearInterval(this.deployInterval);
-    //                     }
-    //             }, (error) => {
-    //                 this.notifyError('An error has occurred, please try again later');
-    //             });
-    //         }, 60000);
-    //     },
-    // },
     computed: {
         deployed: function() {
             return tokenDeploymentStatus.deployed === this.statusProp;
