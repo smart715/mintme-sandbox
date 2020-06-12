246a790858ce16288bb9a9e256b9e975feacb902
ivanhoe <ivan.mudrik@ctemplar.com>
2020-06-12 21:36:23 +0300

    [Version20200214081826.php] fixed default value for buy_depth

--- src/Migrations/Version20200214081826.php
+++ src/Migrations/Version20200214081826.php
@@ -22,7 +22,7 @@ final class Version20200214081826 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE market_status ADD buy_depth VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE market_status ADD buy_depth VARCHAR(255) NOT NULL DEFAULT 0');
     }
 
     public function down(Schema $schema) : void
