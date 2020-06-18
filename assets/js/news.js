1cd13b45cdeb7b68ca3aed2959c2b7e7de96f2ac
denlev <levadnyuk66@gmail.com>
2020-06-18 14:59:18 +0300

    Implemented data-aos and fixed erros

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,9 +1,18 @@
+import AOS from 'aos';
+import Vue from 'vue';
 import NewsImage from './components/NewsImage';
 import store from './storage';
+
+window.Vue = Vue;
+
+AOS.init({
+    easing: 'ease-out-back',
+    duration: 1600,
+    once: true,
+});
 
 new Vue({
     el: '#news-selector',
     store,
     components: {NewsImage},
 });
-
