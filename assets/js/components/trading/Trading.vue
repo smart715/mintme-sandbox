aac2489323620ecc24fee95cc7862f25684f77bc
sambasten <akinwumisamuel@gmail.com>
2020-06-23 09:39:47 +0000

    [vue]timeout on axios request 6s

--- assets/js/components/trading/Trading.vue
+++ assets/js/components/trading/Trading.vue
@@ -475,7 +475,7 @@ export default {
                         this.sendLogs('error', 'Can not update the markets data', err);
                         reject(err);
                     });
-                }, 8000);
+                }, 6000);
             });
         },
         sanitizeMarket: function(marketData) {
