aff5ab30bcaa5a96762db97ddbed8bef2733ab8e
sambasten <akinwumisamuel@gmail.com>
2020-06-27 17:08:05 +0000

    [vue]delay inside fetchdata to delay marketinfo

--- assets/js/components/trading/Trading.vue
+++ assets/js/components/trading/Trading.vue
@@ -405,18 +405,22 @@ export default {
 
             Promise.all([updateDataPromise, conversionRatesPromise.catch((e) => e)])
                 .then((res) => {
-                    if (
-                        Object.keys(this.markets).length === 1
-                        && !this.marketFilters.userSelected
-                        && this.marketFilters.selectedFilter === this.marketFilters.options.deployed.key
-                    ) {
-                        this.marketFilters.selectedFilter = this.marketFilters.options.all.key;
-                        this.fetchData();
-                        return;
-                    }
-                    this.updateDataWithMarkets();
-                    this.loading = false;
-
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            if (
+                                Object.keys(this.markets).length === 1
+                                && !this.marketFilters.userSelected
+                                && this.marketFilters.selectedFilter === this.marketFilters.options.deployed.key
+                            ) {
+                                this.marketFilters.selectedFilter = this.marketFilters.options.all.key;
+                                this.fetchData();
+                                return;
+                            }
+                            this.updateDataWithMarkets();
+                            this.loading = false;
+                            resolve();
+                        }, 1500);
+                    })
                     this.addMessageHandler((result) => {
                         if ('state.update' === result.method) {
                             this.sanitizeMarket(result);
@@ -464,39 +468,37 @@ export default {
                     params.deployed = 1;
                 }
                 this.loading = true;
-                setTimeout(() => {
-                    this.$axios.retry.get(this.$routing.generate('markets_info', params))
-                    .then((res) => {
-                        if (null !== this.markets) {
-                            this.addOnOpenHandler(() => {
-                                const request = JSON.stringify({
-                                    method: 'state.unsubscribe',
-                                    params: [],
-                                    id: parseInt(Math.random().toString().replace('0.', '')),
-                                });
-                                this.sendMessage(request);
+                this.$axios.retry.get(this.$routing.generate('markets_info', params))
+                .then((res) => {
+                    if (null !== this.markets) {
+                        this.addOnOpenHandler(() => {
+                            const request = JSON.stringify({
+                                method: 'state.unsubscribe',
+                                params: [],
+                                id: parseInt(Math.random().toString().replace('0.', '')),
                             });
-                        }
-                        this.currentPage = page;
-                        this.markets = res.data.markets;
-                        this.perPage = res.data.limit;
-                        this.totalRows = res.data.rows;
+                            this.sendMessage(request);
+                        });
+                    }
+                    this.currentPage = page;
+                    this.markets = res.data.markets;
+                    this.perPage = res.data.limit;
+                    this.totalRows = res.data.rows;
 
-                        if (window.history.replaceState) {
-                            // prevents browser from storing history with each change:
-                            window.history.replaceState(
-                                {page}, document.title, this.$routing.generate('trading', {page})
-                            );
-                        }
+                    if (window.history.replaceState) {
+                        // prevents browser from storing history with each change:
+                        window.history.replaceState(
+                            {page}, document.title, this.$routing.generate('trading', {page})
+                        );
+                    }
 
-                        resolve();
-                    })
-                    .catch((err) => {
-                        this.notifyError('Can not update the markets data. Try again later.');
-                        this.sendLogs('error', 'Can not update the markets data', err);
-                        reject(err);
-                    });
-                }, 2000);
+                    resolve();
+                })
+                .catch((err) => {
+                    this.notifyError('Can not update the markets data. Try again later.');
+                    this.sendLogs('error', 'Can not update the markets data', err);
+                    reject(err);
+                });
             });
         },
         sanitizeMarket: function(marketData) {
@@ -728,23 +730,21 @@ export default {
         },
         fetchConversionRates: function() {
             return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    this.$axios.retry.get(this.$routing.generate('exchange_rates'))
-                    .then((res) => {
-                        if (!(res.data && Object.keys(res.data).length)) {
-                            return Promise.reject();
-                        }
+                this.$axios.retry.get(this.$routing.generate('exchange_rates'))
+                .then((res) => {
+                    if (!(res.data && Object.keys(res.data).length)) {
+                        return Promise.reject();
+                    }
 
-                        this.conversionRates = res.data;
-                        resolve();
-                    })
-                    .catch((err) => {
-                        this.$emit('disable-usd');
-                        this.notifyError('Error fetching exchange rates for cryptos. Selecting USD as currency might not work');
-                        this.sendLogs('error', 'Error fetching exchange rates for cryptos', err);
-                        reject();
-                    });
-                }, 2000);
+                    this.conversionRates = res.data;
+                    resolve();
+                })
+                .catch((err) => {
+                    this.$emit('disable-usd');
+                    this.notifyError('Error fetching exchange rates for cryptos. Selecting USD as currency might not work');
+                    this.sendLogs('error', 'Error fetching exchange rates for cryptos', err);
+                    reject();
+                });
             });
         },
         toUSD: function(amount, currency, subunit = false) {
