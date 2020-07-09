f5fb7436ef23d8459618071073f7c0d21bb24f0e
sambasten <akinwumisamuel@gmail.com>
2020-07-09 21:39:42 +0000

    [php] using delayed messaging plugin

--- src/Producer/MarketDelayedProducer.php
+++ src/Producer/MarketDelayedProducer.php
@@ -9,9 +9,8 @@ use PhpAmqpLib\Wire\AMQPTable;
 /** @codeCoverageIgnore */
 class MarketDelayedProducer extends Producer
 {
-    private const MARKET_DELAYED_QUQUE_NAME = 'market-delayed';
-    private const MARKET_EXCHANGE_NAME = 'market';
-    private const MARKET_QUQUE_NAME = 'market';
+    private const MARKET_DELAYED_QUQUE_NAME = 'market';
+    private const MARKET_DELAYED_EXCHANGE_NAME = 'market';
 
     private const QUEUE_IS_PASSIVE      = false;
     private const QUEUE_IS_DURABLE      = true;
@@ -27,24 +26,10 @@ class MarketDelayedProducer extends Producer
         }
 
         $msg = new AMQPMessage($msgBody, array_merge($this->getBasicProperties(), $additionalProperties));
+        $delay = 15000;
+        $headersTable = new AMQPTable(['x-delay' => $delay]);
+        $msg->set('application_headers', $headersTable);
 
-        if (!empty($headers)) {
-            $headersTable = new AMQPTable($headers);
-            $msg->set('application_headers', $headersTable);
-        }
-
-        // declare market queue and bind to exchange
-        $this->getChannel()->queue_declare(
-            self::MARKET_QUQUE_NAME,
-            self::QUEUE_IS_PASSIVE,
-            self::QUEUE_IS_DURABLE,
-            self::QUEUE_IS_EXCLUSIVE,
-            self::QUEUE_IS_AUTO_DELETE,
-            self::QUEUE_IS_NOWAIT,
-        );
-        $this->getChannel()->queue_bind(self::MARKET_QUQUE_NAME, self::MARKET_EXCHANGE_NAME);
-
-        // declare market-delayed queue and bind to market-delayed exchange
         $this->getChannel()->queue_declare(
             self::MARKET_DELAYED_QUQUE_NAME,
             self::QUEUE_IS_PASSIVE,
@@ -52,17 +37,21 @@ class MarketDelayedProducer extends Producer
             self::QUEUE_IS_EXCLUSIVE,
             self::QUEUE_IS_AUTO_DELETE,
             self::QUEUE_IS_NOWAIT,
-            [
-                'x-message-ttl' => ['I', 15000],
-                'x-dead-letter-exchange' => [
-                    'S', self::MARKET_EXCHANGE_NAME, // sends message from market delayed queue to market queise after 15s
-                ],
-            ]
         );
 
-        $this->getChannel()->queue_bind(self::MARKET_DELAYED_QUQUE_NAME, $this->exchangeOptions['name']);
+        $this->getChannel()->exchange_declare(
+            self::MARKET_DELAYED_EXCHANGE_NAME,
+            'x-delayed-mesage',
+            false,
+            true,
+            false,
+            false,
+            false,
+            ['x-delayed-type' => 'fanout'],
+        );
 
-        $this->getChannel()->basic_publish($msg, $this->exchangeOptions['name'], $routingKey);
+        $this->getChannel()->queue_bind(self::MARKET_DELAYED_QUQUE_NAME, self::MARKET_DELAYED_EXCHANGE_NAME);
+        $this->getChannel()->basic_publish($msg, self::MARKET_DELAYED_EXCHANGE_NAME, $routingKey);
 
         $this->logger->debug('[Market] Delayed message published', [
             'amqp' => [
