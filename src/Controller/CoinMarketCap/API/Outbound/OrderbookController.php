444784de1fbc658eaec84c225715f27c907f5349
ivanhoe <ivan.mudrik@ctemplar.com>
2020-07-30 13:25:55 +0300

    [OrderbookController.php] #6193 added endpoint 3 orderbook

--- src/Controller/CoinMarketCap/API/Outbound/OrderbookController.php
+++ src/Controller/CoinMarketCap/API/Outbound/OrderbookController.php
@@ -4,7 +4,7 @@ namespace App\Controller\CoinMarketCap\API\Outbound;
 
 use App\Exception\ApiNotFoundException;
 use App\Exchange\Market;
-use App\Exchange\Market\MarketHandlerInterface;
+use App\Exchange\Trade\TraderInterface;
 use App\Manager\CryptoManagerInterface;
 use App\Manager\TokenManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
@@ -16,51 +16,66 @@ use FOS\RestBundle\Request\ParamFetcherInterface;
  */
 class OrderbookController extends AbstractFOSRestController
 {
-    /** @var MarketHandlerInterface */
-    private $marketHandler;
-
     /** @var CryptoManagerInterface */
     private $cryptoManager;
 
     /** @var TokenManagerInterface */
     private $tokenManager;
 
+    /** TraderInterface */
+    private $trader;
+
     public function __construct(
-        MarketHandlerInterface $marketHandler,
         TokenManagerInterface $tokenManager,
-        CryptoManagerInterface $cryptoManager
+        CryptoManagerInterface $cryptoManager,
+        TraderInterface $trader
     ) {
-        $this->marketHandler = $marketHandler;
         $this->tokenManager = $tokenManager;
         $this->cryptoManager = $cryptoManager;
+        $this->trader = $trader;
     }
     /**
      * Get complete level 2 order book (arranged by best asks/bids) with full depth returned for a given market pair.
      *
      * @Rest\Get("/{market_pair}")
-     * @Rest\QueryParam(name="depth", default=100)
-     * @Rest\QueryParam(name="level", default=3)
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     default="500",
+     *     requirements=@Assert\Range(min="1", max="500"),
+     *     nullable=false,
+     *     allowBlank=false,
+     *     strict=true
+     * )
+     * @Rest\QueryParam(
+     *     name="interval",
+     *     requirements="(0|1)",
+     *     default="0",
+     *     nullable=false,
+     *     allowBlank=false,
+     *     strict=true
+     * )
      * @Rest\View()
      */
-    public function getOrderBook(
-        string $market_pair,
-        ParamFetcherInterface $request
-    ): array {
-        $marketNames = explode('_', $market_pair);
+    public function getOrderBook(ParamFetcherInterface $request, string $marketPair): array
+    {
+        $marketNames = explode('_', $marketPair);
         $base = $marketNames[0] ?? '';
         $quote = $marketNames[1] ?? '';
         $base = $this->cryptoManager->findBySymbol($base);
         $quote = $this->cryptoManager->findBySymbol($quote) ?? $this->tokenManager->findByName($quote);
 
         if (is_null($base) || is_null($quote)) {
-            throw new ApiNotFoundException('Market not found');
+            throw new ApiNotFoundException('Market pair not found');
         }
 
         $market = new Market($base, $quote);
-        $depth = $request->get('depth');
-        $data = $this->marketHandler->getOrdersDepth($market, $depth);
-        $data['timestamp'] = time();
 
-        return $data;
+        return $this->trader->getOrderDepth(
+            $market,
+            [
+                'limit' => (int)$request->get('limit'),
+                'interval' => (int)$request->get('interval'),
+            ]
+        );
     }
 }
