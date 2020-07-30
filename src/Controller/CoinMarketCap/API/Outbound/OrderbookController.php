7ae0c3804b21c5fab90f496c19d3054b52f83a24
ivanhoe <ivan.mudrik@ctemplar.com>
2020-07-30 16:54:58 +0300

    [OrderbookController.php] #6193 added rebranding

--- src/Controller/CoinMarketCap/API/Outbound/OrderbookController.php
+++ src/Controller/CoinMarketCap/API/Outbound/OrderbookController.php
@@ -9,6 +9,7 @@ use App\Exchange\Market\MarketFinderInterface;
 use App\Exchange\Trade\TraderInterface;
 use App\Manager\CryptoManagerInterface;
 use App\Manager\TokenManagerInterface;
+use App\Utils\Converter\RebrandingConverterInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
@@ -35,16 +36,21 @@ class OrderbookController extends AbstractFOSRestController
     /** @var MarketFinderInterface */
     private $marketFinder;
 
+    /** @var RebrandingConverterInterface */
+    private $rebrandingConverter;
+
     public function __construct(
         TokenManagerInterface $tokenManager,
         CryptoManagerInterface $cryptoManager,
         TraderInterface $trader,
-        MarketFinderInterface $marketFinder
+        MarketFinderInterface $marketFinder,
+        RebrandingConverterInterface $rebrandingConverter
     ) {
         $this->tokenManager = $tokenManager;
         $this->cryptoManager = $cryptoManager;
         $this->trader = $trader;
         $this->marketFinder = $marketFinder;
+        $this->rebrandingConverter = $rebrandingConverter;
     }
     /**
      * Get complete level 2 order book (arranged by best asks/bids) with full depth returned for a given market pair.
@@ -72,6 +78,9 @@ class OrderbookController extends AbstractFOSRestController
         $marketPair = explode('_', $market_pair);
         $base = $marketPair[0] ?? '';
         $quote = $marketPair[1] ?? '';
+
+        $base = $this->rebrandingConverter->reverseConvert(mb_strtolower($base));
+        $quote = $this->rebrandingConverter->reverseConvert(mb_strtolower($quote));
 
         $market = $this->marketFinder->find($base, $quote);
 
