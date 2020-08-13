dfd866ef8f434df2f7663ce83012d3743baa433b
victoracho <victoracho.box@gmail.com>
2020-08-13 15:46:10 -0400

    [php] removed nickname wrong label #6488

--- src/Form/Type/NicknameType.php
+++ src/Form/Type/NicknameType.php
@@ -5,7 +5,7 @@ namespace App\Form\Type;
 use App\Validator\Constraints\IsNotBlacklisted;
 use App\Validator\Constraints\UniqueNickname;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
