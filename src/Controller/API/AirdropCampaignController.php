e70dbef87675e918dfae9a1cbf5629dd97851720
sambasten <akinwumisamuel@gmail.com>
2020-08-23 23:58:19 +0000

    [php] fixed backend

--- src/Controller/API/AirdropCampaignController.php
+++ src/Controller/API/AirdropCampaignController.php
@@ -151,7 +151,7 @@ class AirdropCampaignController extends AbstractFOSRestController
             throw new ApiBadRequestException('Token does not have active airdrop campaign.');
         }
 
-        if ($token->getActiveAirdrop()->getId() !== $airdrop->getId()) {
+        if ($token->getActiveAirdrop()->getId() !== $airdrop->getId() || $token->getActiveAirdrop()->getEndDate()->getTimeStamp() < time()) {
             throw new ApiBadRequestException('Current airdrop campaign is finished.');
         }
 
