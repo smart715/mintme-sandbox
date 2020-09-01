6f63b2674e4602f066ceb7765f2a65a621e5d8e7
ivanhoe <243-ivanhoe@users.noreply.gitlab.abchosting.org>
2020-09-01 11:53:46 +0000

    #5698

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,6 +1,24 @@
 import AOS from 'aos';
+import fontawesome from '@fortawesome/fontawesome';
+import fab from '@fortawesome/fontawesome-free-brands';
+import far from '@fortawesome/fontawesome-free-regular';
+import {faSearch, faCog, fas} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon, FontAwesomeLayers} from '@fortawesome/vue-fontawesome';
 import NewsImage from './components/NewsImage';
 import store from './storage';
+
+fontawesome.library.add(fas, far, fab, faSearch, faCog);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+
+new Vue({
+    el: '#news',
+    components: {
+        FontAwesomeIcon,
+        FontAwesomeLayers,
+    },
+});
 
 AOS.init({
     easing: 'ease-out-back',
