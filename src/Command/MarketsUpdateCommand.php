8f1483c64d2ca239752d18bb3614c54282b86ea5
Alejandro Salazar <ale_salazar@outlook.com>
2020-09-03 08:16:55 +0000

    issue #6590

--- src/Command/MarketsUpdateCommand.php
+++ src/Command/MarketsUpdateCommand.php
@@ -4,7 +4,10 @@ namespace App\Command;
 
 use App\Exchange\Factory\MarketFactoryInterface;
 use App\Exchange\Market\MarketHandlerInterface;
+use App\Manager\CryptoManagerInterface;
 use App\Manager\MarketStatusManagerInterface;
+use App\Manager\TokenManagerInterface;
+use App\Utils\Converter\RebrandingConverterInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,12 +27,27 @@ class MarketsUpdateCommand extends Command
     /** @var MarketFactoryInterface */
     private $marketFactory;
 
+    /** @var CryptoManagerInterface */
+    private $cryptoManager;
+
+    /** @var TokenManagerInterface */
+    private $tokenManager;
+
+    /** @var RebrandingConverterInterface */
+    private $rebrandingConverter;
+
     public function __construct(
         MarketStatusManagerInterface $marketStatusManager,
-        MarketFactoryInterface $marketFactory
+        MarketFactoryInterface $marketFactory,
+        CryptoManagerInterface $cryptoManager,
+        TokenManagerInterface $tokenManager,
+        RebrandingConverterInterface $rebrandingConverter
     ) {
         $this->marketStatusManager = $marketStatusManager;
         $this->marketFactory = $marketFactory;
+        $this->cryptoManager = $cryptoManager;
+        $this->tokenManager = $tokenManager;
+        $this->rebrandingConverter = $rebrandingConverter;
         parent::__construct();
     }
 
@@ -37,6 +55,7 @@ class MarketsUpdateCommand extends Command
     {
         $this
             ->setDescription('Update markets with information from viabtc server')
+            ->addArgument('market', InputArgument::OPTIONAL, 'The market to update (e.g. BTC/MINTME)')
         ;
     }
 
@@ -45,6 +64,18 @@ class MarketsUpdateCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
+        /** @var string $market */
+        $market = $input->getArgument('market');
+
+        if ($market) {
+            return $this->updateOne($this->rebrandingConverter->reverseConvert($market), $io);
+        }
+
+        return $this->updateAll($io);
+    }
+
+    protected function updateAll(SymfonyStyle $io): int
+    {
         $markets = $this->marketFactory->createAll();
         $io->progressStart(count($markets));
 
@@ -55,6 +86,45 @@ class MarketsUpdateCommand extends Command
 
         $io->progressFinish();
         $io->success('Markets updated');
+
+        return 0;
+    }
+
+    protected function updateOne(string $market, SymfonyStyle $io): int
+    {
+        $market = explode('/', $market);
+
+        if (2 !== count($market)) {
+            $io->error('Invalid argument market');
+
+            return 1;
+        }
+
+        $base = $this->cryptoManager->findBySymbol($market[0]);
+        $quote = $this->cryptoManager->findBySymbol($market[1]) ?? $this->tokenManager->findByName($market[1]);
+
+        if (!$base) {
+            $io->error('Base crypto not found');
+
+            return 1;
+        }
+
+        if (!$quote) {
+            $io->error('Quote crypto or token not found');
+
+            return 1;
+        }
+
+        $market = $this->marketFactory->create($base, $quote);
+
+        try {
+            $this->marketStatusManager->updateMarketStatus($market);
+            $io->success('Market updated');
+        } catch (\Throwable $e) {
+            $io->error('Error: '.$e->getMessage());
+
+            return 1;
+        }
 
         return 0;
     }
