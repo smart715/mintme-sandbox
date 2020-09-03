9aa07b67f1879236a42797126867664a4f646bf6
ivanhoe <ivan.mudrik@ctemplar.com>
2020-09-03 13:31:55 +0300

    [php tests] #6193 added SummaryControllerTest.php, fixed symbols in assets test

--- tests/Controller/Dev/API/V2/SummaryControllerTest.php
+++ tests/Controller/Dev/API/V2/SummaryControllerTest.php
@@ -5,18 +5,17 @@ namespace App\Tests\Controller\Dev\API\V2;
 use App\Entity\Token\Token;
 use App\Tests\Controller\WebTestCase;
 
-class AssetsControllerTest extends WebTestCase
+class SummaryControllerTest extends WebTestCase
 {
     public function testGetAssets(): void
     {
-        $this->client->request('GET', '/dev/api/v2/open/assets');
+        $this->client->request('GET', '/dev/api/v2/open/summary');
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
 
         $res = json_decode((string)$this->client->getResponse()->getContent(), true);
 
-        $this->assertArrayHasKey(Token::MINTME_SYMBOL, $res);
-        $this->assertArrayHasKey(Token::BTC_SYMBOL, $res);
-        $this->assertArrayHasKey(Token::ETH_SYMBOL, $res);
+        $this->assertArrayHasKey(Token::MINTME_SYMBOL . '_' . Token::BTC_SYMBOL, $res[0]);
+        $this->assertArrayHasKey(Token::MINTME_SYMBOL . '_' . Token::ETH_SYMBO, $res[1]);
     }
 }
