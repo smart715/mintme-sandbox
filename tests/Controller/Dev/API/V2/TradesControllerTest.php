8f618ad4d304a3ffd76db08ce71d420eb0285f25
ivanhoe <ivan.mudrik@ctemplar.com>
2020-09-03 14:02:22 +0300

    [php tests] #6193 added trades, ticker tests, fixed const visibility

--- tests/Controller/Dev/API/V2/TradesControllerTest.php
+++ tests/Controller/Dev/API/V2/TradesControllerTest.php
@@ -5,11 +5,11 @@ namespace App\Tests\Controller\Dev\API\V2;
 use App\Entity\Token\Token;
 use App\Tests\Controller\WebTestCase;
 
-class OrderbookControllerTest extends WebTestCase
+class TradesControllerTest extends WebTestCase
 {
-    private const URL = '/dev/api/v2/open/orderbook';
+    private const URL = '/dev/api/v2/open/trades';
 
-    public function testGetOrderbook(): void
+    public function testGetTrades(): void
     {
         $markets = [
             Token::MINTME_SYMBOL . '_' . Token::BTC_SYMBOL,
