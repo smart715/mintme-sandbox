1d170ec16e49e67f8fb5df28a1e8440d5518ae8c
Yaroslav Harbolinskyi <yaroslav.artistsoft@gmail.com>
2020-09-03 13:43:31 +0300

    [vue, css] Debug code. #6232

--- assets/js/components/posts/Posts.vue
+++ assets/js/components/posts/Posts.vue
@@ -3,7 +3,10 @@
         <div class="card-header">
             <slot name="title">Posts</slot>
         </div>
-        <div class="card-body posts">
+        <div
+            class="card-body posts"
+            ref="postsContainer"
+        >
             <template v-if="posts.length > 0">
                 <post v-for="(n, i) in postsCount"
                     :post="posts[i]"
@@ -15,7 +18,7 @@
                     :logged-in="loggedIn"
                 />
             </template>
-            <div v-else class="text-centered">
+            <div v-else class="position-absolute top-50">
                 The token creator has not added any posts yet.
             </div>
             <a v-if="showReadMore"
@@ -23,7 +26,7 @@
                 :href="readMoreUrl"
                 @click.prevent="goToPosts"
             >
-                All Posts
+                More posts
             </a>
         </div>
     </div>
@@ -59,6 +62,23 @@ export default {
         return {
             readMoreUrl: this.$routing.generate('token_show', {name: this.tokenName, tab: 'posts'}),
         };
+    },
+    mounted() {
+        document.addEventListener('DOMContentLoaded', () => {
+            let container1 = document.querySelectorAll('.posts')[0];
+            console.error('Case 1: ', container1.clientHeight, container1.scrollHeight, container1.offsetHeight);
+
+            let posts1 = this.$refs.postsContainer;
+            console.error('Case 2: ', posts1.clientHeight, posts1.scrollHeight, posts1.offsetHeight);
+        });
+
+        this.$nextTick(() => {
+            let container2 = document.querySelectorAll('.posts')[0];
+            console.error('Case 3: ', container2.clientHeight, container2.scrollHeight, container2.offsetHeight);
+
+            let posts2 = this.$refs.postsContainer;
+            console.error('Case 4: ', posts2.clientHeight, posts2.scrollHeight, posts2.offsetHeight);
+        });
     },
     computed: {
         postsCount() {
