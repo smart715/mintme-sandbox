af919e6bc7552768201b8757054dc3472f2e2994
Yigit Ozmen <yigit.ozmen@gmail.com>
2020-10-28 00:58:45 +0300

    [new migration file added]

--- src/Migrations/Version20201027175444.php
+++ src/Migrations/Version20201027175444.php
@@ -19,8 +19,13 @@ final class Version20201027175444 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_Posts_Tokens');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_Posts_Tokens FOREIGN KEY (token_id) REFERENCES token(id) ON DELETE CASCADE');
+
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_Comments_Posts');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_Comments_Posts FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE');
+
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_Comments_Users');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_Comments_Users FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE');
     }
 
