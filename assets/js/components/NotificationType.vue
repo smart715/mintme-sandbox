42986deb94eb76c9770acd3054e8f36c3845776b
Gustavo Diaz <gustavo4581@gmail.com>
2020-11-06 12:09:57 -0400

    #6674

--- assets/js/components/NotificationType.vue
+++ assets/js/components/NotificationType.vue
@@ -28,10 +28,10 @@ export default {
         translationsContext: function(notification) {
             if (this.notificationType.withdrawal !== notification.type && this.notificationType.deposit !== notification.type) {
                 return {
-                    urlProfile: this.$routing.generate('profile-view', {nickname: notification.jsonData.profile}),
-                    profile: notification.jsonData.profile,
-                    tokenName: notification.jsonData.tokenName,
-                    urlToken: this.$routing.generate('token_show', {name: notification.jsonData.tokenName}),
+                    urlProfile: this.$routing.generate('profile-view', {nickname: JSON.parse(notification.jsonData).profile}),
+                    profile: JSON.parse(notification.jsonData).profile,
+                    tokenName: JSON.parse(notification.jsonData).tokenName,
+                    urlToken: this.$routing.generate('token_show', {name: JSON.parse(notification.jsonData).tokenName}),
                 };
             }
             return {
