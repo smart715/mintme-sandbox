72c4cb0660834c90ac0458978ddcb93562aebca1
Yigit Ozmen <yigit.ozmen@gmail.com>
2020-12-10 14:13:56 +0300

    [suggestions fixed]

--- src/Controller/API/PostsController.php
+++ src/Controller/API/PostsController.php
@@ -17,7 +17,6 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\View\View;
-use phpDocumentor\Reflection\Types\False_;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -105,6 +104,10 @@ class PostsController extends AbstractFOSRestController
     /**
      * @Rest\View()
      * @Rest\Get("/list/{tokenName}", name="list_posts", options={"expose"=true})
+     * @Rest\RequestParam(name="tokenName", nullable=True)
+     * @param string|null $tokenName
+     * @return View
+     * @throws ApiNotFoundException
      */
     public function list(?string $tokenName = null): View
     {
