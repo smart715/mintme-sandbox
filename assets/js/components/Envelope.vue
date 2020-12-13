43e391cc119f8930155eb7ee447f9597e949b19b
karen <kmelkumyan11@gmail.com>
2020-12-13 22:50:10 +0200

    [vue,js,twig] add Envelope component #6936

---
+++ assets/js/components/Envelope.vue
@@ -0,0 +1,15 @@
+<template>
+    <font-awesome-icon icon="envelope" size="lg" />
+</template>
+
+<script>
+
+export default {
+    name: 'Envelope',
+    props: {
+        isOwner: Boolean,
+        dmMinAmount: String,
+        getQuoteBalance: Boolean,
+    },
+};
+</script>
