67eec967b4d491a8534df7dc4eb6f1fcd9c65fe7
karen <kmelkumyan11@gmail.com>
2020-12-14 21:32:05 +0200

    [vue,js,twig] add NotificationMixin #6936

--- assets/js/components/Envelope.vue
+++ assets/js/components/Envelope.vue
@@ -1,5 +1,10 @@
 <template>
-    <font-awesome-icon icon="envelope" size="lg" @click="checkDirectMessage" />
+    <a
+        href="getDirectMessageLink"
+        class="chat-envelope d-block text-white pl-2 pr-2"
+    >
+        <font-awesome-icon icon="envelope" size="lg" @click="checkDirectMessage" />
+    </a>
 </template>
 
 <script>
@@ -13,10 +18,14 @@ import {NotificationMixin} from '../mixins';
         isOwner: Boolean,
         dmMinAmount: String,
         getQuoteBalance: Boolean,
+        tokenName: String,
     },
     methods: {
         checkDirectMessage: function() {
             this.notifyError('To send direct message you need to have 100 SuperCoins tokens');
+        },
+        getDirectMessageLink: function() {
+            return this.$routing.generate('chat');
         },
     },
 };
