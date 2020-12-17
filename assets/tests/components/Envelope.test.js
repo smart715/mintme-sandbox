f189aab3ffea2353f61f84ae9d284ae8f95135b8
karen <kmelkumyan11@gmail.com>
2020-12-17 23:50:23 +0200

    [vue,js,twig] add test #6936

--- assets/tests/components/Envelope.test.js
+++ assets/tests/components/Envelope.test.js
@@ -1,7 +1,7 @@
 import {shallowMount, createLocalVue} from '@vue/test-utils';
 import Envelope from '../../js/components/Envelope';
 
-const $routing = {generate: (val, params) => val};
+const $routing = {generate: (val, params) => val + (params ? params.tokenName : '')};
 
 /**
  * @return {Wrapper<Vue>}
@@ -45,5 +45,35 @@ describe('Envelope', () => {
         });
 
         expect(wrapper.find('font-awesome-icon').attributes('icon')).toBe('envelope');
+    });
+
+    it('should compute showEnvelope correctly', () => {
+        const wrapper = shallowMount(Envelope, {
+            localVue: mockVue(),
+            propsData: {
+                loggedIn: true,
+                isOwner: false,
+                dmMinAmount: '100',
+                getQuoteBalance: '0',
+                tokenName: 'Foo',
+            },
+        });
+
+        expect(wrapper.vm.showEnvelope).toBe(true);
+    });
+
+    it('should compute getDirectMessageLink for owner correctly', () => {
+        const wrapper = shallowMount(Envelope, {
+            localVue: mockVue(),
+            propsData: {
+                loggedIn: true,
+                isOwner: true,
+                dmMinAmount: '100',
+                getQuoteBalance: '0',
+                tokenName: 'Foo',
+            },
+        });
+
+        expect(wrapper.vm.getDirectMessageLink).toBe('chat');
     });
 });
