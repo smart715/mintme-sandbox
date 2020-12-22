407339a9084c99ec6e2af90803e915b1f9efed85
denlev <levadnyuk66@gmail.com>
2020-12-22 15:56:32 +0200

    DonationManager and fetch user related Donations

--- src/Repository/DonationRepository.php
+++ src/Repository/DonationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Donation;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +12,22 @@ class DonationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Donation::class);
+    }
+
+    public function findAllUserRelated(User $user): array
+    {
+        $tokenOwner = $user->getProfile()->getMintmeToken()->getOwner();
+
+        $result = $this->createQueryBuilder('donation')
+            ->where('donation.donor = :donor')
+            ->setParameter('donor', $user);
+
+        if ($tokenOwner) {
+            $result
+                ->orWhere('donation.tokenCreator = :tokenCreator')
+                ->setParameter('tokenCreator', $tokenOwner);
+        }
+
+        return $result->getQuery()->getResult();
     }
 }
