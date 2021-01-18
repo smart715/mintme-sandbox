7b1dd0aff9d8c3200c8325d7a7f1725253cd7d80
karen <kmelkumyan11@gmail.com>
2021-01-18 15:34:59 +0200

    [vue] add additional testing #7039

--- assets/js/components/trade/Trade.vue
+++ assets/js/components/trade/Trade.vue
@@ -287,7 +287,6 @@ export default {
                         id: data.id,
                     }))
                     .then((res) => {
-                        res.data.accounted = true;
                         orders.push(res.data);
                         this.saveOrders(orders, isSell);
                         this.ordersUpdated = true;
