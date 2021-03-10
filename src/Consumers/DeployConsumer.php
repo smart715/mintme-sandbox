24abe9ff993a185b0fb7d7db7f878e7e50f4b0cc
denlev <levadnyuk66@gmail.com>
2021-03-11 00:38:15 +0200

    Mailer instance for the deploy event

--- src/Consumers/DeployConsumer.php
+++ src/Consumers/DeployConsumer.php
@@ -8,10 +8,10 @@ use App\Entity\DeployTokenReward;
 use App\Entity\Token\LockIn;
 use App\Entity\Token\Token;
 use App\Entity\User;
+use App\Events\DeployCompletedEvent;
 use App\Events\TokenEvent;
 use App\Events\TokenEvents;
 use App\Exchange\Balance\BalanceHandlerInterface;
-use App\Mailer\MailerInterface;
 use App\SmartContract\Model\DeployCallbackMessage;
 use Doctrine\ORM\EntityManagerInterface;
 use Money\Currency;
@@ -40,16 +40,13 @@ class DeployConsumer implements ConsumerInterface
 
     private EventDispatcherInterface $eventDispatcher;
 
-    private MailerInterface $mailer;
-
     public function __construct(
         LoggerInterface $logger,
         int $coinbaseApiTimeout,
         EntityManagerInterface $em,
         BalanceHandlerInterface $balanceHandler,
         DeployCostFetcherInterface $deployCostFetcher,
-        EventDispatcherInterface $eventDispatcher,
-        MailerInterface $mailer
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->logger = $logger;
         $this->coinbaseApiTimeout = $coinbaseApiTimeout;
@@ -57,7 +54,6 @@ class DeployConsumer implements ConsumerInterface
         $this->balanceHandler = $balanceHandler;
         $this->deployCostFetcher = $deployCostFetcher;
         $this->eventDispatcher = $eventDispatcher;
-        $this->mailer = $mailer;
     }
 
     /** {@inheritdoc} */
@@ -149,19 +145,15 @@ class DeployConsumer implements ConsumerInterface
 
             return false;
         }
-        
-        try {
-            $this->mailer->checkConnection();
-            $this->mailer->sendOwnTokenDeployedMail($user, $token->getName(), $clbResult->getTxHash());
-        } catch (\Throwable $e) {
-            $this->logger->error(
-                "Couldn't send deploy completed e-mail to user {$user->getEmail()}. Reason: {$e->getMessage()}"
-            );
-        }
+
+        /** @psalm-suppress TooManyArguments */
+        $this->eventDispatcher->dispatch(
+            new DeployCompletedEvent($user, $token->getName(), $clbResult->getTxHash()),
+            DeployCompletedEvent::NAME
+        );
 
         /** @psalm-suppress TooManyArguments */
         $this->eventDispatcher->dispatch(new TokenEvent($token), TokenEvents::DEPLOYED);
-
 
         return true;
     }
