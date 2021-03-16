bd73f136ef6b8e4f74c28fb40dea7b610ffbb863
Fernando Esparragoza <fernando.fjep@gmail.com>
2021-03-16 14:02:36 -0400

    [sass,vue] using css class instead #6854

--- assets/js/components/posts/Posts.vue
+++ assets/js/components/posts/Posts.vue
@@ -19,10 +19,7 @@
                           @go-to-post="$emit('go-to-post', $event)"
                     />
                 </template>
-                <div v-else
-                    :class="{ 'position-absolute top-50': tokenPage }"
-                    style="transform: translateY(-50%);"
-                >
+                <div v-else class="absolute-center">
                     {{ $t('post.not_any_post') }}
                 </div>
             </div>
