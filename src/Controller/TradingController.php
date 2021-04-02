ab057b3bb555e796b36e6f45b360915e0d8f4f75
Fernando Esparragoza <fernando.fjep@gmail.com>
2021-04-02 15:08:25 -0400

    [php] unused import #6815

--- src/Controller/TradingController.php
+++ src/Controller/TradingController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use App\Utils\Converter\RebrandingConverterInterface;
 
 class TradingController extends Controller
 {
