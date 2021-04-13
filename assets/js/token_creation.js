27b0d94a39097b9edc436b47df107e3d7db327bd
karen <kmelkumyan11@gmail.com>
2021-04-14 02:34:44 +0300

    [js] set error message #6839

--- assets/js/token_creation.js
+++ assets/js/token_creation.js
@@ -110,10 +110,7 @@ new Vue({
                         });
                         frm.submit();
                     }
-                }, (err) => {
-                    console.log(err.response);
-                    this.notifyError(err.response.data.message);
-                });
+                }, (err) => this.notifyError(err.response.data));
         },
         tokenInvalid: function(e) {
             e.target.setCustomValidity('Invalid token name.');
