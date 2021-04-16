bcd77afe6de3022c18f6f1696fe2252679889df0
uosama <osamaesmail2@gmail.com>
2021-04-16 12:18:26 +0000

    #7127

--- tests/Controller/API/CryptosControllerTest.php
+++ tests/Controller/API/CryptosControllerTest.php
@@ -12,16 +12,18 @@ class CryptosControllerTest extends WebTestCase
     {
         $this->register($this->client);
 
-        $this->client->request('GET', '/api/cryptos/rates');
+        $this->client->request('GET', self::LOCALHOST . '/api/cryptos/rates');
         $res = json_decode((string)$this->client->getResponse()->getContent(), true);
 
-        $this->assertCount(3, $res);
+        $this->assertCount(4, $res);
         $this->assertArrayHasKey('BTC', $res);
         $this->assertArrayHasKey('WEB', $res);
+        $this->assertArrayHasKey('ETH', $res);
+        $this->assertArrayHasKey('USDC', $res);
         $this->assertArrayHasKey('BTC', $res['BTC']);
         $this->assertArrayHasKey('USD', $res['BTC']);
-        $this->assertArrayHasKey('ETH', $res);
         $this->assertArrayHasKey('ETH', $res['ETH']);
+        $this->assertArrayHasKey('USD', $res['USDC']);
     }
 
     public function testGetBalance(): void
@@ -29,14 +31,14 @@ class CryptosControllerTest extends WebTestCase
         $email = $this->register($this->client);
 
         $this->sendWeb($email, '133000000000000000000');
-        $this->client->request('GET', '/api/cryptos/WEB/balance');
+        $this->client->request('GET', self::LOCALHOST . '/api/cryptos/WEB/balance');
         $res = json_decode((string)$this->client->getResponse()->getContent(), true);
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
         $this->assertEquals('133.000000000000000000', $res);
 
         $this->deposit($email, '150000', Symbols::BTC);
-        $this->client->request('GET', '/api/cryptos/BTC/balance');
+        $this->client->request('GET', self::LOCALHOST . '/api/cryptos/BTC/balance');
         $res = json_decode((string)$this->client->getResponse()->getContent(), true);
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
