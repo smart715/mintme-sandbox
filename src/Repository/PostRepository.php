2d08507d77cbd9b764bd7c28a4b3c7ea46044cde
Dim bag <codefree57@gmail.com>
2021-05-15 01:21:38 +0300

    [php,js,yml] Add home page for every user issue-#7289

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -2,49 +2,13 @@
 
 namespace App\Repository;
 
-use App\Entity\Post;
-use App\Entity\User;
-use App\Exchange\Balance\BalanceHandlerInterface;
-use App\Manager\TokenManagerInterface;
-use App\Utils\Symbols;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
-use Money\Currency;
-use Money\Money;
+use Doctrine\ORM\EntityRepository;
 
-class PostRepository extends ServiceEntityRepository
+class PostRepository extends EntityRepository
 {
-    private TokenManagerInterface $tokenManager;
-
-    private BalanceHandlerInterface $balanceHandler;
-
-    public function __construct(
-        ManagerRegistry $registry,
-        TokenManagerInterface $tokenManager,
-        BalanceHandlerInterface $balanceHandler
-    ) {
-        $this->tokenManager = $tokenManager;
-        $this->balanceHandler = $balanceHandler;
-
-        parent::__construct($registry, Post::class);
-    }
-
     /** @codeCoverageIgnore */
-    public function findRecentPostsOfUser(User $user, int $page = 0, int $max = 10): array
+    public function findRecentPostsByTokens(array $tokens, int $page = 0, int $max = 10): array
     {
-        $tokens = [];
-
-        foreach ($user->getTokens() as $token) {
-            $available = $this->tokenManager->getRealBalance(
-                $token,
-                $this->balanceHandler->balance($user, $token)
-            )->getAvailable();
-
-            if ($available->greaterThanOrEqual(new Money(0, new Currency(Symbols::TOK)))) {
-                $tokens[] = $token;
-            }
-        }
-
         return $this->createQueryBuilder('post')
             ->where('post.token IN (:tokens)')
             ->andWhere('post.createdAt BETWEEN :thirtyDays AND :today')
