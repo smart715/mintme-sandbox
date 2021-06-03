51db7e7692f28291994b05f23d5edbdbf685aa3f
Fernando Esparragoza <fernando.fjep@gmail.com>
2021-06-03 19:03:58 -0400

    [php] removing testing logs #7366

--- src/Manager/UserTokenManager.php
+++ src/Manager/UserTokenManager.php
@@ -8,23 +8,18 @@ use App\Entity\UserToken;
 use App\Repository\UserTokenRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Money\Money;
-use Psr\Log\LoggerInterface;
 
 class UserTokenManager implements UserTokenManagerInterface
 {
     private UserTokenRepository $repository;
     private EntityManagerInterface $entityManager;
-    private LoggerInterface $logger;
 
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        LoggerInterface $logger
-    ) {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
         /** @var UserTokenRepository $repository */
         $repository = $this->entityManager->getRepository(UserToken::class);
         $this->repository = $repository;
-        $this->logger = $logger;
     }
 
     public function findByUserToken(User $user, Token $token): ?UserToken
@@ -43,39 +38,17 @@ class UserTokenManager implements UserTokenManagerInterface
 
         $userToken = $this->findByUserToken($user, $token);
 
-        $this->logger->info('usertoken', [
-            'email' => $user->getEmailAuthRecipient(),
-            'isBalanceZero' => $balance->isZero(),
-            'exists' => !!$userToken,
-            'userToken' => $userToken,
-        ]);
-
         if (!$userToken && !$balance->isZero()) {
             $userToken = (new UserToken())->setToken($token)->setUser($user);
             $this->entityManager->persist($userToken);
             $user->addToken($userToken);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            $this->logger->info('usertoken added');
         } elseif ($userToken && $balance->isZero()) {
             $this->entityManager->remove($userToken);
             $user->removeToken($userToken);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            $this->logger->info('usertoken removed');
         }
-
-        $userToken2 = $this->repository->findByUserToken(
-            $user->getId(),
-            $token->getId()
-        );
-
-        $this->logger->info('usertoken 2', [
-            'email' => $user->getEmailAuthRecipient(),
-            'isBalanceZero' => $balance->isZero(),
-            'exists' => !!$userToken,
-            'userToken' => $userToken,
-            'userTokens' => $user->getTokens(),
-        ]);
     }
 }
