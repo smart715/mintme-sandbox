e0b9686bc8e5aa2a84da7ae7a8107fa280d16092
denlev <levadnyuk66@gmail.com>
2021-06-14 20:09:42 +0300

    Remove is_token from BNB

--- src/Migrations/Version20210614170848.php
+++ src/Migrations/Version20210614170848.php
@@ -10,24 +10,24 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210607131512 extends AbstractMigration
+final class Version20210614170848 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Set Binance Coin to is tradable and to is token';
+        return 'Set Binance Coin to tradable';
     }
 
     public function up(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('UPDATE crypto SET tradable = 1, is_token = 1 WHERE name = \'Binance Coin\';');
- }
+        $this->addSql('UPDATE crypto SET tradable = 1 WHERE name = \'Binance Coin\';');
+    }
 
     public function down(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('UPDATE crypto SET tradable = 0, is_token = 0 WHERE name = \'Binance Coin\';');
+        $this->addSql('UPDATE crypto SET tradable = 0 WHERE name = \'Binance Coin\';');
     }
 }
