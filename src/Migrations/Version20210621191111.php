44568d815b85d355958233c52acfb65c1108f117
Eena <lenka20231@gmail.com>
2021-06-22 19:32:44 +0300

    [css] Adding a new post with transations is broken-7410

--- src/Migrations/Version20210621191111.php
+++ src/Migrations/Version20210621191111.php
@@ -21,15 +21,11 @@ final class Version20210621191111 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE news__post CHANGE es_abstract es_abstract LONGTEXT DEFAULT NULL, CHANGE ar_abstract ar_abstract LONGTEXT DEFAULT NULL, CHANGE fr_abstract fr_abstract LONGTEXT DEFAULT NULL, CHANGE pl_abstract pl_abstract LONGTEXT DEFAULT NULL, CHANGE pt_abstract pt_abstract LONGTEXT DEFAULT NULL, CHANGE ru_abstract ru_abstract LONGTEXT DEFAULT NULL, CHANGE ua_abstract ua_abstract LONGTEXT DEFAULT NULL, CHANGE es_content es_content LONGTEXT DEFAULT NULL, CHANGE ar_content ar_content LONGTEXT DEFAULT NULL, CHANGE fr_content fr_content LONGTEXT DEFAULT NULL, CHANGE pl_content pl_content LONGTEXT DEFAULT NULL, CHANGE pt_content pt_content LONGTEXT DEFAULT NULL, CHANGE ru_content ru_content LONGTEXT DEFAULT NULL, CHANGE ua_content ua_content LONGTEXT DEFAULT NULL');
-
-
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE news__post CHANGE es_abstract es_abstract VARCHAR(255) DEFAULT NULL, CHANGE ar_abstract ar_abstract VARCHAR(255) DEFAULT NULL, CHANGE fr_abstract fr_abstract VARCHAR(255) DEFAULT NULL, CHANGE pl_abstract pl_abstract VARCHAR(255) DEFAULT NULL, CHANGE pt_abstract pt_abstract VARCHAR(255) DEFAULT NULL, CHANGE ru_abstract ru_abstract VARCHAR(255) DEFAULT NULL, CHANGE ua_abstract ua_abstract VARCHAR(255) DEFAULT NULL, CHANGE es_content es_content VARCHAR(255) DEFAULT NULL, CHANGE ar_content ar_content VARCHAR(255) DEFAULT NULL, CHANGE fr_content fr_content VARCHAR(255) DEFAULT NULL, CHANGE pl_content pl_content VARCHAR(255) DEFAULT NULL, CHANGE pt_content pt_content VARCHAR(255) DEFAULT NULL, CHANGE ru_content ru_content VARCHAR(255) DEFAULT NULL, CHANGE ua_content ua_content VARCHAR(255) DEFAULT NULL');
-
-
     }
 }
