d9f95ba5057428ab8ea2a7850611a6475572409e
Eena <lenka20231@gmail.com>
2021-06-28 15:35:09 +0300

    [css] Top menu is not being adjusted correctly issue-7420

--- assets/js/components/token/TokenSearcher.vue
+++ assets/js/components/token/TokenSearcher.vue
@@ -103,7 +103,7 @@ export default {
     },
     computed: {
         inputClass: function() {
-            return (this.isLoggedIn ? 'research-input form-control no-bg-img' : 'search-input form-control no-bg-img');
+            return this.isLoggedIn ? 'research-input form-control no-bg-img' : 'search-input form-control no-bg-img';
         },
     },
 };
